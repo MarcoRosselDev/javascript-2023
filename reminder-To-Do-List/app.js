9aa477f408ef50d4f1b794053789b39365c55bf3
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-06 15:10:05 -0300

    html structure

---
+++ reminder-To-Do-List/app.js
@@ -0,0 +1,10 @@
+/*
+  <input type="text" class="myInput" />
+      <button class="printButton">print</button>
+      <button class="deleteButton">delete</button>
+      <button class="deleteAllButton">delete all</button>
+    </form>
+
+    <!-- print input content -->
+    <div class="container"></div>
+*/
