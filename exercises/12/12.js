71e42f55ae122b306772341b6bb38725a8379591
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-07 18:51:18 -0300

    12.2

--- exercises/12/12.js
+++ exercises/12/12.js
@@ -1,9 +1,26 @@
 // Ejercicio 12: comportamiento del alcance o contexto funcional.
 
-function first() {
+function function1() {
   var a = 2;
+
+  function function2() {
+    var b = 5;
+
+    function function5() {
+      console.log(a, b);
+    }
+  }
 }
 
-function second() {
+function function2() {
   var a = 7;
+
+  function function4() {
+    console.log(a);
+  }
 }
+
+// console.log(a);
+
+function1();
+function2();
