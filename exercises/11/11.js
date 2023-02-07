3390b9f5bc25c3a2c89d8ff8e92646546c7d81b2
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-07 16:02:10 -0300

    ejercicio 11.1

---
+++ exercises/11/11.js
@@ -0,0 +1,11 @@
+// Ejercicio 11: Resolver problema de referencia de variables.
+
+// Error tipico:
+// ReferenceError: <<identificador>> is not defined
+
+function saludar() {
+  var mensaje = "este es un mensaje ejecutado desde una funcion";
+  console.log(mensaje);
+}
+
+console.log(mensaje);
