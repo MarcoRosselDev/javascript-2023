dcd036e8ea2cc41ff50c08ed80a188735646561b
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-07 15:55:10 -0300

    exercise 10

---
+++ exercises/10/app.js
@@ -0,0 +1,11 @@
+// ejercicio 10: Declarar una ficnion usando la instruiccion function.
+
+function exercise(a, b) {
+  var sumar = a + b;
+  return sumar;
+}
+
+console.log(exercise(1, 2));
+console.log(exercise(10, 2));
+console.log(exercise(1, 20));
+console.log(exercise(6, 5));
