b5b0d8960b549206eac7d8b745c825fff4eb5ea1
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-07 09:31:36 -0300

    reminder 2

--- reminder-To-Do-List/app.js
+++ reminder-To-Do-List/app.js
@@ -1,19 +1,21 @@
-/*
-  <input type="text" class="myInput" />
-      <button class="printButton">print</button>
-      <button class="deleteButton">delete</button>
-      <button class="deleteAllButton">delete all</button>
-    </form>
-
-    <!-- print input content -->
-    <div class="container"></div>
-*/
-
 const myInput = document.querySelector(".myInput");
-
+// botones
 const printButton = document.querySelector(".printButton");
+const deleteButton = document.querySelector(".deleteButton");
+const deleteAllButton = document.querySelector(".deleteAllButton");
+// div
 const container = document.querySelector(".container");
 
+// push on array object to save on localStorage
+let save = [];
+
 printButton.addEventListener("click", () => {
-  container.innerHTML = `<h1>${myInput.value}</h1>`;
+  save.push(myInput.value);
+  myInput.value = ""; // clean te input to write another task
+
+  console.log(save);
 });
+
+const printDOM = () => {
+  container.innerHTML = `<h1>${myInput.value}</h1>`;
+};
