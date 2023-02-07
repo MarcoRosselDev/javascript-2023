2016d68f3ac1e34c57875d4d31e8a14ef3c6f530
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-07 09:21:07 -0300

    form faunded

--- reminder-To-Do-List/app.js
+++ reminder-To-Do-List/app.js
@@ -8,16 +8,12 @@
     <!-- print input content -->
     <div class="container"></div>
 */
-console.log("hola");
 
 const myInput = document.querySelector(".myInput");
-const ipt = document.querySelector(".ipt");
 
 const printButton = document.querySelector(".printButton");
 const container = document.querySelector(".container");
 
-console.log(printButton);
-
 printButton.addEventListener("click", () => {
-  return (container.innerHTML = `<h1>${myInput.value}</h1>`);
+  container.innerHTML = `<h1>${myInput.value}</h1>`;
 });
