e56a174057fb7dba3b9c1494f57b725a1abfc503
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-08 12:31:33 -0300

    ejercicio 21

---
+++ exercises/21-operadores-relacionales/21.js
@@ -0,0 +1,15 @@
+//
+// ejercicio 21 : Uso de operadores realcionales.
+
+// <. <=, >, >=, ==, ===
+
+// == compara contenido pero no tipo de dato
+// === compara contenido y tipo de dato
+
+let numero = 5;
+
+if (numero < 7) {
+  console.log(`${numero} es menor a 7`);
+} else {
+  console.log(`${numero} es mayor a 7`);
+}
