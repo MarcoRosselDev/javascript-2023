fdabb10227efac79c35bdbaeef04bdc31afd522a
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-08 12:21:51 -0300

    ejercicio 19

---
+++ exercises/19-undefined-o-null/19.js
@@ -0,0 +1,12 @@
+// Ejercicio 19: Validar si una variable es undefined o null.
+
+let numero = undefined;
+
+if (numero === undefined) console.log('(1) La variable "numero" es undefined');
+if (numero === null) console.log('(2) La variable "numero" es null');
+
+numero = null;
+if (numero === null) console.log('(3) La variable "numero" es null');
+
+// ! (bang)
+if (!numero) console.log('(4) La variable "numero" es null');
