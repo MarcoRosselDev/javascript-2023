36d86d7fc982dc63a67b234fd1ca8d1f04db3aad
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-08 17:40:08 -0300

    ejercicio 27

---
+++ exercises/27-if-tipo-data-is-string/27.js
@@ -0,0 +1,34 @@
+// Ejercicio 27: Averiguar si una variable es de tipo string.
+
+let a = String("JavaSript");
+
+let b = "ES6";
+let c = new Date();
+let d = 5;
+
+if (typeof a === "string") {
+  console.log("la varable a es una cadena de caracteres.");
+} else {
+  console.log("la varable a no es una cadena de caracteres.");
+}
+//b
+
+if (typeof b === "string") {
+  console.log("la varable b es una cadena de caracteres.");
+} else {
+  console.log("la varable b no es una cadena de caracteres.");
+}
+//c
+
+if (typeof c === "string") {
+  console.log("la varable c es una cadena de caracteres.");
+} else {
+  console.log("la varable c no es una cadena de caracteres.");
+}
+//d
+
+if (typeof d === "string") {
+  console.log("la varable d es una cadena de caracteres.");
+} else {
+  console.log("la varable d no es una cadena de caracteres.");
+}
