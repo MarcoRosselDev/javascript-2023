55d9f0a66752af3394a3557a2792cf0057b1bbce
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-08 18:37:45 -0300

    ejercicio 33

---
+++ exercises/33-aritmetica-basica/33.js
@@ -0,0 +1,10 @@
+// ejercicio 33: Aritmetica basica entre objetos date.
+
+let time1 = Date.now();
+console.log(time1);
+
+let time2 = Date.now();
+console.log(time2);
+
+let result = time2 - time1;
+console.log(result);
