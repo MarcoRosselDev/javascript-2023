fb3ef7af14deb8824fec2aef30853e89dee5b321
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-07 22:56:00 -0300

    15

--- exercises/15-instancias-array/15.js
+++ exercises/15-instancias-array/15.js
@@ -6,3 +6,15 @@
 [2, 3, 5, 7, 11]; // esto es una lista en foma literal de crarla
 
 const primos = new Array(2, 3, 5, 7, 11); // mecanismo por medio de constructor
+
+function restar(a, b) {
+  return a - b;
+}
+
+let solution = restar(5, 2);
+
+console.log(solution);
+
+let restarFn = new Function("a", "b", "return a - b"); // esta es por medio de constructor
+
+console.log(restarFn(2, 3));
