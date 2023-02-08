d2cb1efc8c57a07966e1f0c9d32ad4f9d082ca9b
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-08 12:48:58 -0300

    ejercicio 23

---
+++ exercises/23-promocionar-valore-a-logicos/23.js
@@ -0,0 +1,28 @@
+//
+// ejercicio 23: Promocionar valores a tipo logico (booleano).
+
+// en ingles se le conoce como coercionar o coersing
+
+console.log(Boolean(-0)); // false
+console.log(Boolean(0)); // false
+console.log(Boolean(1)); // true
+console.log(Boolean(new Number(0))); // true
+console.log(Boolean(7)); // true
+console.log(Boolean(-7)); // true
+
+console.log();
+
+console.log(Boolean(true)); // true
+console.log(Boolean(false)); // false
+
+console.log();
+
+console.log(Boolean(undefined)); // false
+console.log(Boolean(null)); // false
+
+console.log();
+
+console.log("strings");
+console.log(Boolean("")); // false
+console.log(Boolean(new String())); // true
+console.log(Boolean(new String(""))); // true
