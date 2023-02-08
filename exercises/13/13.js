41854e5c8437ca46a01885b75d4f4ed1ed193975
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-07 22:18:23 -0300

    13

---
+++ exercises/13/13.js
@@ -0,0 +1,15 @@
+// ejercicio 13: Creacion de una constante con la sentencia const.
+
+var nombre = "Marco";
+let lenguaje = "javaScript";
+const PI = 3.1415;
+
+// si se decine como termino variable es una locacion de memoria del sistema para guardar informacion
+
+const estudiante = { id: 5, nombre: "Marco" };
+
+console.log(estudiante);
+
+estudiante.id = 7;
+
+console.log(estudiante);
