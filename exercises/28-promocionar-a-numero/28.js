de83b213d711c1b71cc80f885cda47104021ea92
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-08 17:51:01 -0300

    ejercicio 28

---
+++ exercises/28-promocionar-a-numero/28.js
@@ -0,0 +1,27 @@
+// Ejercicio 28: Promocionar valores a tipo numerico (Number).
+
+let cadena = "1.414216";
+const CONSTANTE_PITAGORICA = Number(cadena); // pasamos el string de la variable cadena a numero
+
+console.log(typeof CONSTANTE_PITAGORICA);
+// number
+
+//----------------------------------------------------
+// que pasa si combertimos a numero valores booleanos?
+console.log(Number(false)); // false === 0
+console.log(Number(true)); // true === 1
+
+//----------------------------------------------------
+// otras pruebas de valores
+
+console.log(Number(undefined)); // NaN
+console.log(Number(null)); // 0
+console.log(Number(NaN)); // NaN
+
+//----------------------------------------------------
+console.log("stringssss");
+// que pasa con los strings
+const fu1 = () => {};
+console.log(Number("")); // 0
+console.log(Number("hola")); // NaN
+console.log(Number(fu1)); // NaN
