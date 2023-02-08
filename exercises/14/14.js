c10e4438813ace4b2350405657d63a0a065a8bc4
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-07 22:29:04 -0300

    14

---
+++ exercises/14/14.js
@@ -0,0 +1,9 @@
+// Ejercicio 14: Crear y usar literales de diferentes tipos de datos.
+
+let nombre = new String("Marco Rossel"); //  <---- esto es una
+
+nombre = "Marco Rossel"; // <----- esto es una literal
+let puntaje = 123; // <----- esto es una literal
+
+let jugando = new Boolean(true);
+jugando = false; // literal
