bd6347c68df1c008799df385123b9c111941da88
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-08 14:16:04 -0300

    ejercicio 25

---
+++ exercises/25-consultar-repre-valid-de-logic/25.js
@@ -0,0 +1,17 @@
+// Ejercicio 25: Consultar las representaciones validas de valores logicos
+
+if (0) {
+  console.log("0 es true");
+} else {
+  console.log("0 es false");
+}
+// 0 es false por que 0 en si es un booleano false
+// booleanos falsos son el 0, -0
+// cualesquier otro numero mayor 0 menor a 0 es true
+
+if (null) {
+  console.log("null es true");
+} else {
+  console.log("null es false");
+}
+// null es false
