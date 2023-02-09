faa07db62cbe2b3b7a401f32f6c31ce7b5562532
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-09 19:08:09 -0300

    ejercicio 53

---
+++ exercises/53-multiplicacion/53.js
@@ -0,0 +1,11 @@
+// ejercicio 53: Uso del operador de producto o multiplicacion.
+
+// obviando lo obvio pasamos a los casos interesantes
+
+console.log("2" * "2"); //4
+console.log("4" * 3); //12
+console.log("a" * 3); //NaN
+
+console.log();
+// lo que hace javaScript es
+// NaN * 2 ---------> no puede multiplicar si no es numero
