29ff2138722399c28db617ef8e669a996ed5b2ff
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-08 21:40:04 -0300

    ejercicio 35

---
+++ exercises/35-validar-tipo-date/35.js
@@ -0,0 +1,24 @@
+// ejercicio 35: validar si una variable es de tipo Date.
+
+let x = new Date();
+let y = "javaScript";
+let z = 2.31;
+
+if (x instanceof Date) {
+  console.log();
+  console.log("x es tipo date");
+} else {
+  console.log("x no es tipo date");
+}
+if (y instanceof Date) {
+  console.log();
+  console.log("y es tipo date");
+} else {
+  console.log("y no es tipo date");
+}
+if (z instanceof Date) {
+  console.log();
+  console.log("z es tipo date");
+} else {
+  console.log("z no es tipo date");
+}
