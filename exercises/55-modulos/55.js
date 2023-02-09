30de7c3aedbf40f92a82b7168b187dd34a6328a9
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-09 19:26:57 -0300

    ejercicio 55

---
+++ exercises/55-modulos/55.js
@@ -0,0 +1,17 @@
+//
+// ejercicio 55: Uso del operador de modulo o resto de la division
+
+// MOD, %     <---------- representacion
+
+// el modulo es lo que queda de la division
+
+let a = 5;
+let b = 7;
+
+console.log(a % b); // 5/7 ---> no se puede por lo que es = 5
+console.log(b % a); // 7/5 ---> si se puede por lo que es = 2 (lo que queda)
+console.log(10 % 4); // 10/4 = 8 y quedan 2 por lo que es = 2
+console.log(9 % 10); // = 9
+// cuando el numerador es menor que el divisor el modulo es el numerador
+
+// en el caso de la coercion si aplica , por lo que intentara coercionar texto a numero si no aplica NaN
