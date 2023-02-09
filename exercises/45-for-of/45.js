d1cd4967e7c0d6ca165307a9453bc69b0e786f1e
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-09 16:11:59 -0300

    ejercicio 45

---
+++ exercises/45-for-of/45.js
@@ -0,0 +1,15 @@
+//
+// Ejercicio 45: Recorrer un arreglo por medio de for...of
+
+let animales = ["🐄", "🐎", "🦙", "🐢", "🦘"];
+
+for (let a of animales) {
+  console.log(a);
+}
+// esto de hecho funciona, loool
+
+// 🐄
+// 🐎
+// 🦙
+// 🐢
+// 🦘
