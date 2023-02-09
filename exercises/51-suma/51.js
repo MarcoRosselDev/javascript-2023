80d7b43b93dd7341812de964f121dad1da18628c
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-09 18:55:16 -0300

    ejercicio 51

---
+++ exercises/51-suma/51.js
@@ -0,0 +1,14 @@
+// ejercicio 51: uso del operador de adicion
+
+let a = 2;
+let b = 3;
+
+let suma = a + b;
+
+console.log(suma); // 5
+console.log(suma + 5); // 10
+
+let x = "2";
+let y = 3;
+console.log(x + y); // 23
+// tiene prioriadad la concatenacion
