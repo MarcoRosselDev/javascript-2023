a129a5ca9c6c67a5130f2976332e7bcb00818d19
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-08 21:52:18 -0300

    ejercicio 36

---
+++ exercises/36-objeto/36.js
@@ -0,0 +1,19 @@
+// ejercicio 36: Crear y asignar propiedades a un objeto
+
+let computador = {}; // esta declarado
+computador.procesador = "Intel Core i7";
+computador.ram = 32;
+computador.marca = "MSI";
+computador.board = "ASUS";
+
+console.log(computador);
+
+computador.procesador = "Intel Core i7 Octava Generacion";
+
+console.log();
+console.log(computador);
+
+computador.sistema_operativo = "window 11";
+
+console.log();
+console.log(computador);
