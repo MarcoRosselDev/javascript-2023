33c560ee08fb16dddc374c33525300901763392d
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-10 13:59:30 -0300

    ejercicio 63

---
+++ exercises/60-100/63-desigualdad-estricta/63.js
@@ -0,0 +1,9 @@
+// ejercicio 63: Uso del operador estrictamente diferente de.
+
+// !==
+
+//true != 1       --------> false
+//true !== 1       -------> true
+
+// la igualdad estricta como su nombre lo dice es mas estricta,
+// esta considera el tipo de dato no solo su contenido
