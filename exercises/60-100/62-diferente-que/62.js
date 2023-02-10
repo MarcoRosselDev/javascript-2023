73e1deda74bb923c4aa4455ed0fa76e40436367d
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-10 13:54:35 -0300

    ejercicio 62

---
+++ exercises/60-100/62-diferente-que/62.js
@@ -0,0 +1,16 @@
+// ejercicio 62: Uso del operador diferente de
+
+// !=
+
+if (5 != "5") {
+  console.log('5 es distinyo de "5"');
+}
+// no aplica por que nos es estricto como !==
+
+let a = [];
+let b = [];
+
+if (a != b) {
+  console.log("a es distinto de b");
+}
+// si aplica lol
