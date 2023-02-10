92064fb865510d10207a479637592bff2ebaaa5e
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-10 17:17:49 -0300

    ejercicio 71

---
+++ exercises/60-100/71-void/71.js
@@ -0,0 +1,10 @@
+// ejercicio 71: uso del operador void
+
+// undefined
+console.log(undefined);
+//console.log(window.undefined); no aplica por que no es un navegador como chrom
+
+// se requiere de void cuando queremos que se aplique como referencia undefined
+// osea que se ejecute la operacion requerida como una suma o un cambio de estilo
+// y luego no retorne nada, como undefined
+// 71.html como ejemplo del uso de void
