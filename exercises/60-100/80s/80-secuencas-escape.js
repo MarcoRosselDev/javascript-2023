bebba939b2beafa66193fc3c8b5c9ab084805df7
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-10 23:22:25 -0300

    ejercicio 80

---
+++ exercises/60-100/80s/80-secuencas-escape.js
@@ -0,0 +1,18 @@
+// ejercicio 80: uso de secuencias de escape en cadenas de caracteres.
+
+// \n  -----> salto de linea
+// \t  -----> tabulacion
+// \'  ----->
+// \"  ----->
+
+let cadena = "javaScript\nes\n\ttremendo";
+
+console.log(cadena);
+
+// respuesta:
+// javaScript
+// es
+//    tremendo
+
+// ejemplo de uso de los escapes de commillas, que quisas algun dia pueden ser utiles
+// cadena = "la version mas reciente de \"javaScript\" es la ES6"
