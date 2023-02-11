c743be4060e8b97efe5c633e9106b37e1e957d9f
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-10 21:10:28 -0300

    ejercicio 74

---
+++ exercises/60-100/71-void/74-comprobar-indice-array.js
@@ -0,0 +1,6 @@
+// ejercicio 74: Uso del operador in para comprobar si un indice esta presente en un arreglo
+
+let lenguajes = ["JavaScript", "Python", "Java", "PHP"];
+
+console.log(lenguajes.length); //4
+console.log(0 in lenguajes); // true
