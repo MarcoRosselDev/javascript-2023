945e4370db46645a5f3315002e7385bafcd8b617
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-12 14:23:47 -0300

    ejercicio 88

---
+++ exercises/60-100/80s/88-iterar-por-caracter.js
@@ -0,0 +1,30 @@
+// ejercicio 88: iterar por todos los caracteres de una cadena de caracteres.
+
+let texto = "javaScript es genial!";
+
+for (let i in texto) {
+  console.log(texto[i]);
+}
+// resultado
+
+// j
+// a
+// v
+// a
+// S
+// c
+// r
+// i
+// p
+// t
+
+// e
+// s
+
+// g
+// e
+// n
+// i
+// a
+// l
+// !
