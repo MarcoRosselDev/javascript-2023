9e7fd03e4e2973e26433c93f6f68449191ac52f3
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-12 20:47:29 -0300

    ejercicio 101 - 102

---
+++ exercises/101-200/101-toLowerCase-toUpperCase.js
@@ -0,0 +1,11 @@
+// ejercicio 101: alternar entre minusculas y mayusculas don toLowerCase y toUpperCase.
+
+let cadena = "Karl Marx: Ilusion y Grandeza";
+
+console.log(cadena.toLowerCase());
+// karl marx: ilusion y grandeza
+
+console.log(cadena.toUpperCase());
+// KARL MARX: ILUSION Y GRANDEZA
+
+// nada que decir, solo es eso.
---
+++ exercises/101-200/102-trim.js
@@ -0,0 +1,10 @@
+// ejercicio 102: remover el espacio de una cadena de caracteres con el metodo trim.
+
+//        John Ortiz Ordo;ez
+// en casos en los que el formato es inapropiado como arriba
+// es util eliminar esos espacioscon trim.
+
+let nombre = "          Marco Rossel N.       ";
+
+console.log(nombre);
+console.log(nombre.trim()); // se removie el espacio al principio y al final.
