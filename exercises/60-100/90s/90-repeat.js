0cbc2c6a959bdb8a786276900f15b12473f97131
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-12 14:35:23 -0300

    ejercicio 90

---
+++ exercises/60-100/90s/90-repeat.js
@@ -0,0 +1,9 @@
+// ejercicio 90: repetir una cadena de caracteres por medio del metodo repeat.
+
+let cadena = "javaScript";
+
+console.log(cadena); // javaScript
+console.log(cadena.repeat(2)); //javaScriptjavaScript
+
+// repeat solo hacepta numero positivos a partir del 0,
+// obviamente 0 no imprimira nada y -1 lanzara un error.
