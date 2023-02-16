70bde044e250e8bc9f076e97bd2abe380dd80a27
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-16 05:07:01 -0300

    ejercicio 117

---
+++ exercises/101-200/117-calcular-raiz-cuadrada.js
@@ -0,0 +1,5 @@
+// ejercicio 117: calcular la raiz cuadrada de un numero con la funcion Math.sqrt.
+
+console.log(Math.sqrt(2)); // 1.4142135623730951
+console.log(Math.sqrt(25)); // 5
+console.log(Math.sqrt(100)); // 10
