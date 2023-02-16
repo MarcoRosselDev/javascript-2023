03f123eb816512e9999ec395cd9e9c925bc64585
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-16 05:03:30 -0300

    ejercicio 116

---
+++ exercises/101-200/116-math-min-max.js
@@ -0,0 +1,6 @@
+// ejercicio 116: obterner el minimo y el maximo de un conjunto de numeros
+
+let numeros = [7, 11, 3, -4, 6, -22, 5, 514, 1, 5, 0];
+
+console.log(`el valor minimo del arreglo numeros es ${Math.min(...numeros)}`); // -22
+console.log(`el valor maximo del arreglo numeros es ${Math.max(...numeros)}`); // 514
