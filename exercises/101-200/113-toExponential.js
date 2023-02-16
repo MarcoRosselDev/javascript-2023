0c9d3428c8e317bc72601767ca6bdc62ec736385
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-16 04:36:06 -0300

    ejercicio 113 toExponential

---
+++ exercises/101-200/113-toExponential.js
@@ -0,0 +1,20 @@
+// ejercicio 113: uso de la funcion toExponential de la clase Number.
+
+function expo(x, f) {
+  return Number.parseFloat(x).toExponential(f);
+}
+
+console.log(expo(123456, 2));
+// Expected output: "1.23e+5"
+
+console.log(expo("123456"));
+// Expected output: "1.23456e+5"
+
+console.log(expo("oink"));
+// Expected output: "NaN"
+
+// output
+
+// 1.23e5
+// 1.23456e5
+// NaN
