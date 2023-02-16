7d8bbc63fd66c8d62a54b9495eb7d24961116944
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-16 20:13:22 -0300

    ejercicio 118 - 119

---
+++ exercises/101-200/110-redondedos.js
@@ -0,0 +1,9 @@
+// ejercicio 119: estidiar la diferencia entre las funciones Math.floor y Math.ceil
+
+// Math.floor(): Redondea el entero menor mas cercano
+// Math.ceil(): Redondea el entero mayor mas cercano
+
+let valor = 3.6;
+
+console.log(Math.floor(valor)); // 3
+console.log(Math.ceil(valor)); // 4
---
+++ exercises/101-200/118-raiz-cubica.js
@@ -0,0 +1,4 @@
+// ejercicio 118: Calcular la raiz cubica de un numero con la funcion Math.cbrt.
+
+console.log(Math.cbrt(27)); // 3
+console.log(Math.cbrt(-27)); // -3
