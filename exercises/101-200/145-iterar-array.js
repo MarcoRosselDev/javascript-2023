97e2a2e36d8c6f12b42ca5350ba299724098dfbf
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-16 23:03:42 -0300

    ejercicio 145

---
+++ exercises/101-200/145-iterar-array.js
@@ -0,0 +1,19 @@
+// ejercicio 145 : iterar por cada uno de los elementos de un arreglo
+
+let numeros = [1, 4, 6, 11, 6, 1, 6, 7, 89, 2];
+
+for (let i = 0; i < numeros.length; i++) {
+  console.log(numeros[i]);
+}
+// salida
+
+// 1
+// 4
+// 6
+// 11
+// 6
+// 1
+// 6
+// 7
+// 89
+// 2
