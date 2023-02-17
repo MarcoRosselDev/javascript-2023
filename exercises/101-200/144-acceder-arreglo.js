8eb382b2d84374735f14eb5dda82d5cfa1991470
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-16 23:00:48 -0300

    ejercicio 144

---
+++ exercises/101-200/144-acceder-arreglo.js
@@ -0,0 +1,6 @@
+// ejercicio 144: acceder a los elementos de un arreglo.
+
+let lenguajes = ["Java", "JavaScript", "C++", "Python"];
+
+console.log(lenguajes[0]); // Java
+console.log(lenguajes[lenguajes.length - 1]); // - 1 por que los arrays se cuentan desde 0 y length se cuenta desde 1
