263a34ce0fd029a8c2cd388cc03e94e69b3857f3
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-16 22:55:49 -0300

    ejercicio 143

---
+++ exercises/101-200/143-push.js
@@ -0,0 +1,12 @@
+// ejercicio 143: agregar elementos a un arreglo con el metodo push.
+
+let lenguajes = ["java", "JavaScript", "c++", "python"];
+
+console.log(lenguajes); // [ 'java', 'JavaScript', 'c++', 'python' ]
+console.log(lenguajes.length); // 4
+
+console.log();
+
+lenguajes.push("PHP");
+console.log(lenguajes); // [ 'java', 'JavaScript', 'c++', 'python', 'PHP' ]
+console.log(lenguajes.length); // 5
