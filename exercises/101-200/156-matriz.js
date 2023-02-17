211edfcc01b97f27bd625ca6646ee59dc7bc28b5
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-17 05:46:58 -0300

    ejercicio 156 matriz

---
+++ exercises/101-200/156-matriz.js
@@ -0,0 +1,15 @@
+// ejercicio 156: crear un arreglo bidimensional
+
+let matriz = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+// muestrame el numero 5
+console.log(matriz[1][1]);
+// muestrame el numero 9
+console.log(matriz[2][2]);
+
+// notece que JavaScript no reconose esto como una matriz como la conosemos nosotros
+// solo procesa esto como un arreglo dentro de otro arreglo.
