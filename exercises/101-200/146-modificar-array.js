6e6c0f64ab7ecfde6654a7901dbd29e335124ce4
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-16 23:06:56 -0300

    ejercicio 146

---
+++ exercises/101-200/146-modificar-array.js
@@ -0,0 +1,10 @@
+// ejercicio 146: modificar el contenido de un arreglo
+
+let lenguajes = ["Java", "JavaScript", "C++", "Python"];
+
+console.log(lenguajes); // [ 'Java', 'JavaScript', 'C++', 'Python' ]
+
+lenguajes[2] = "C";
+
+console.log();
+console.log(lenguajes); // [ 'Java', 'JavaScript', 'C', 'Python' ]
