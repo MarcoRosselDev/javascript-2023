490fd80730cd60c43a92aee5d04d0b4dd606dd0a
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-16 22:53:13 -0300

    ejercicio 142

---
+++ exercises/101-200/142-arreglo-literal-or-class.js
@@ -0,0 +1,10 @@
+// ejercicio 142: crear arreglos con la literal o con la clase array
+
+let numerosPares = []; // literal
+let numerosImpares = new Array(); // class array
+
+console.log(typeof numerosPares); // object
+console.log(typeof numerosImpares); // object
+
+console.log(numerosPares instanceof Array); // true
+console.log(numerosImpares instanceof Array); // true
