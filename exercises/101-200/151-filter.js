725fd03fc7a26fd3408a19928aada14a22aa12c5
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-17 03:35:01 -0300

    ejercicio 151 filter 2

--- exercises/101-200/151-filter.js
+++ exercises/101-200/151-filter.js
@@ -10,3 +10,34 @@ let numerosImpares = numeros.filter((valor) => {
 
 console.log(numeros); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 console.log(numerosImpares); // [ 1, 3, 5, 7, 9 ]
+
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
+
+const result = words.filter((word) => word.length > 6);
+
+console.log(result);
+// Expected output: Array ["exuberant", "destruction", "present"]
+
+//-----------------------------------------------------------------------
+// filter es muy util en general
+
+var fruits = ["apple", "banana", "grapes", "mango", "orange"];
+
+/**
+ * Filtra la matríz en función de un criterio de búsqueda (query)
+ */
+function filterItems(query) {
+  return fruits.filter(function (el) {
+    return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+  });
+}
+
+console.log(filterItems("ap")); // ['apple', 'grapes']
+console.log(filterItems("an")); // ['banana', 'mango', 'orange']
