623e57a6bd995d333f34b7df3810a476a678cc22
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-17 23:40:25 -0300

    ejercicio 170 slice

---
+++ exercises/101-200/170-slice.js
@@ -0,0 +1,9 @@
+// ejercicio 170: obtener un subconjunto de elementos de un arreglo
+
+const lenguajes = ["Java", "C", "C++", "JavaScript", "Python"];
+
+console.log(lenguajes);
+
+const revanada = lenguajes.slice(3, 5); // [ 'JavaScript', 'Python' ]
+
+console.log(revanada);
