0d7d1a87f62be1657a93e7551aede3744fe0d8ae
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-17 22:29:40 -0300

    ejercicio 163 concat

---
+++ exercises/163-concat.js
@@ -0,0 +1,10 @@
+// ejercicio 163: Agregar un arreglo al final de otro arreglo.
+
+const numero1 = [0, 1, 2, 3, 4];
+const nuemro2 = [5, 6, 7, 8, 9];
+
+const concatenacion = numero1.concat(nuemro2);
+
+console.log(concatenacion);
+
+// al arreglo al que se le aplica .concat tiene la primera posicion en orden
