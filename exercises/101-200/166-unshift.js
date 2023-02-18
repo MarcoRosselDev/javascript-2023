60d81d03fff122a2f844d0c33e8fae6b6264a093
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-17 23:14:11 -0300

    ejercicio 166 unshift

---
+++ exercises/101-200/166-unshift.js
@@ -0,0 +1,8 @@
+// ejercicio 166: copiar varios elementos al principio de un arreglo con unshift
+
+let numeros = [4, 5, 6];
+// queremos agregar al principio los elementos 1, 2, 3
+
+numeros.unshift(1, 2, 3);
+
+console.log(numeros); // [ 1, 2, 3, 4, 5, 6 ]
