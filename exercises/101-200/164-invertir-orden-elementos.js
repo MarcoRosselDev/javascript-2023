6a04fb1928b2a9021bab3d07d9d83ce2aad358a2
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-17 22:32:58 -0300

    ejercicio 164 reverse()

---
+++ exercises/101-200/164-invertir-orden-elementos.js
@@ -0,0 +1,9 @@
+// ejercicio 164: Invertir el orden de los elementos de un arreglo.
+
+const numeros = [1, 3, 4, 6, 8];
+
+console.log(numeros); // [ 1, 3, 4, 6, 8 ]
+
+numeros.reverse();
+
+console.log(numeros); // [ 8, 6, 4, 3, 1 ]
