148d8461195104f5206d7ff3e854cc1fe8836b2c
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-17 22:09:32 -0300

    ejercicio 161 toString

---
+++ exercises/101-200/161-toString.js
@@ -0,0 +1,9 @@
+// ejercicio 161: Crear la representacion de cadena de caracteres de una matriz con toString
+
+const numreos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(numreos.toString()); // 0,1,2,3,4,5,6,7,8,9
+
+console.log(typeof numreos.toString()); // string
+
+// devuelve todo el contenido de un arreglo en formato string plano.
