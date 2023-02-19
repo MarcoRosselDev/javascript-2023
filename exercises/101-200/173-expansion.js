9ec16db8c8b983728ae8fc35b79bdbb7e3bc4549
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-18 21:02:36 -0300

    ejercicio 173  operador expansion ...

---
+++ exercises/101-200/173-expansion.js
@@ -0,0 +1,19 @@
+// ejercicio 173: uso basico del operador de expansion ...
+
+const sumar = (a, b, c) => {
+  return a + b + c;
+};
+
+// simulamos que queremos pasarle los valores de un arreglo como 'parametros'.
+
+const numeros = [1, 4, 6];
+
+console.log(sumar(...numeros)); // 11
+
+// tambien sirve como concatenador de arreglos
+
+const masNumeros = [3, 7, 9];
+
+const numerosConcatenados = [...numeros, ...masNumeros];
+
+console.log(numerosConcatenados); // [ 1, 4, 6, 3, 7, 9 ]
