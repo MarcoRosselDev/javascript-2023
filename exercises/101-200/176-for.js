2e92981762f0e2c068ee47a9fd3c876960a828e4
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-18 21:33:56 -0300

    ejercicio 176 for

---
+++ exercises/101-200/176-for.js
@@ -0,0 +1,13 @@
+// ejercicio 176 : iterar en orden los elementos de un arreglo con el ciclo for
+
+const lenguajes = ["Java", "C", "C++", "JavaScript", "Python"];
+
+for (let i = 0; i < lenguajes.length; i++) {
+  console.log(lenguajes[i]);
+}
+
+// Java
+// C
+// C++
+// JavaScript
+// Python
