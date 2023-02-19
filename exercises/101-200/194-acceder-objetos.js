f27a8681f293a6c5a297508edc5efe9e72de75a4
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-19 05:33:57 -0300

    ejercicio 194

---
+++ exercises/101-200/194-acceder-objetos.js
@@ -0,0 +1,17 @@
+// ejercicio 194: crear y acceder propiedades de un objeto
+
+const persona = {};
+
+console.log(persona);
+
+persona["nombre"] = "Daniela";
+persona["correo-e"] = "daniela@gmail.com";
+
+console.log(persona);
+
+persona.edad = 26;
+
+console.log(persona);
+
+persona.edad = 28;
+console.log(persona);
