bbac23a25728d7d58b9461c2b620155b5e8857b6
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-18 21:47:32 -0300

    ejercicio 178 for of

---
+++ exercises/101-200/178-for-of.js
@@ -0,0 +1,42 @@
+// ejercicio 178: iterar por los valores de un arreglo o una cadena con el ciclo for..of
+
+const lenguajes = ["Java", "C", "C++", "JavaScript", "Python"];
+
+for (let l of lenguajes) {
+  console.log(l);
+}
+// Java
+// C
+// C++
+// JavaScript
+// Python
+
+const texto = "JavaScript es divertido";
+
+for (let p of texto) {
+  console.log(p);
+}
+
+// J
+// a
+// v
+// a
+// S
+// c
+// r
+// i
+// p
+// t
+
+// e
+// s
+
+// d
+// i
+// v
+// e
+// r
+// t
+// i
+// d
+// o
