bbe2192ee2acdc496f500d47b078f91a9e841f4a
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-19 05:28:37 -0300

    ejercicio 193

---
+++ exercises/101-200/193-crear-objetos-literales-y-contructores.js
@@ -0,0 +1,14 @@
+// ejecicio 193: crear objeto por medio de literales y construictores.
+
+// constructor
+const persona1 = new Object();
+persona1.nombre = "Marco";
+persona1.apellido = "Rossel";
+persona1.edad = 29;
+
+console.log(persona1); // { nombre: 'Marco', apellido: 'Rossel', edad: 29 }
+
+// Literal
+
+const persona2 = { nombre: "Eva", apellido: "Nahuelpan", edad: 62 };
+console.log(persona2); // { nombre: 'Eva', apellido: 'Nahuelpan', edad: 62 }
