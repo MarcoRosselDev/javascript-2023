3d940c44750a0d59e9bf139a15b50d235140b7e2
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-18 21:55:37 -0300

    ejercicio 181 sort

---
+++ exercises/101-200/181-sort.js
@@ -0,0 +1,8 @@
+// ejecicio 181: ordenar los elementos de un arreglo con la funcion sort.
+
+let numeros = [1, 5, 7, 2, 8, 4, 9, 1, 0];
+
+console.log(numeros.sort());
+// [0, 1, 1, 2, 4, 5, 7, 8, 9];
+
+// tambien aplica para arreglos de texto, esto considera el orden alfavetico
