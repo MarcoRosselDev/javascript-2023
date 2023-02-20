84fad57ce5ab50531d895ea9585646608f74cedc
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-20 12:29:55 -0300

    ejercicio 219 set add

---
+++ exercises/201-300/219-add-multiples.js
@@ -0,0 +1,7 @@
+// ejercicio 219: agregar multiples elementos a un conjunto con la funcion add().
+
+const numeros = new Set();
+
+numeros.add(1).add(2).add(3).add(4).add(5);
+
+console.log(numeros); // Set(5) { 1, 2, 3, 4, 5 }
