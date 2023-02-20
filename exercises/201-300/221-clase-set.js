f1811b1a9f5f77df297e857297b0a8a0b912b60a
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-20 12:45:25 -0300

    ejercicio 221 set

---
+++ exercises/201-300/221-clase-set.js
@@ -0,0 +1,7 @@
+// ejercicio 221: inicializar un conjunto al momento de crear un objeto de la clase Set.
+
+const numeros = new Set([1, 1, 1, 4, 3, 5, 7, 0, 3, 3, 2, 7, 3]);
+
+console.log(numeros); // Set(7) { 1, 4, 3, 5, 7, 0, 2 }
+
+console.log(numeros.size); // 7
