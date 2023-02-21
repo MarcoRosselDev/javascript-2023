fe9a96707f9848d60be883f1f6931b01ecf4924a
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-21 15:21:30 -0300

    ejercicio 239 IIFE

---
+++ exercises/201-300/239-IIFE/239.js
@@ -0,0 +1,7 @@
+// ejercicio 239: invocar una funcion IIFE una vez se cargue un documento HTML.
+
+(function () {
+  alert("La pagina se ha cargado");
+
+  document.body.style.backgroundColor = "pink";
+})();
