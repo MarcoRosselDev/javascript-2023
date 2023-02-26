32b47f935250ecd3f9fff6f9c8ae7de5dcef435b
Marco Rossel <andresmarcorossel@gmail.com>
2023-02-26 16:55:46 -0400

    ejercicio 297 replace

---
+++ exercises/201-300/297-replace.js
@@ -0,0 +1,8 @@
+// ejercicio 297: usar la funcion replace para reemplazar texto en una cadena de caracteres.
+
+let frase = 'javascript es un lenguaje de programacion que se ejecuta en un navegador Web.'; // texto explisito o  plano
+
+console.log(frase);
+
+frase = frase.replace('javascript', 'JavaScript') 
+console.log(frase);
