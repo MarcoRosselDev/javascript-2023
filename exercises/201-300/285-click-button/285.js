96a05dcf9b60146cc75d2ce76a693839a5715436
Marco Rossel <andresmarcorossel@gmail.com>
2023-02-25 20:44:33 -0400

    ejercicio 285 DOM

---
+++ exercises/201-300/285-click-button/285.js
@@ -0,0 +1,6 @@
+// ejercicio 285: responder el evento click de un elemento button.
+
+function activarFuncion() {
+    let encabezado = document.getElementById('encabezado')
+    encabezado.innerHTML = "JavaScript - Ejercicio 285"
+}
