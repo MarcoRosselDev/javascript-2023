13a7ae32ea72dce63301455cbafc04f2c7af3bbb
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-18 17:47:39 -0300

    mdn documentation | APIs browser y de 3th

--- mozilla-mdn/que-es-javascript.js
+++ mozilla-mdn/que-es-javascript.js
@@ -3,4 +3,29 @@
 Las API son conjuntos de bloques de construccion de codigo listos para usar quq permiten a un desarrollador implementar programas que ded otro modo serian dificiles o imposibles dde implementar.
 Hacen lo mismo para lo programacion que lso kits de muebles prefabricados para la construccion de viviendas --- es mucho mas facil tomar paneles precortados y atornillarlos para hacer una estntaria que elaborar el disegnio tu mismo,  que ir y encontrar la madera correcta, cortar todos los paneles del tamagnio y la forma correctos, buscar los tornillo del tamagnio correcto y luego juntalos para hacer una estanteria.
 
+Generalmente se dividen en dos categorias.
+
+-Browser APIs       -- APIs del navegador
+-3rd party APIs     -- APIs de terceros
+
+Los APIs del navegador |
+    estan integradas al navegador web y puedo exponer datos del entorno informatico circundatnte o realizar tareas complejas y utiles.
+    Ejempolos:
+        API del DOM |
+            permite mainipular HTML y CSS, crear, eliminar y cambiar el HTML, aplicar dinamicamente nuevos estilos a la pagina, etc.
+        API de Geolocalizacion |
+            recupera informacion geografica.
+        APIs Canvas y WebGL |
+            permiten crear graficos animados en 2D y 3D.
+        APIs de audio y video |
+            like zoom o reproductores de audio y viedo.
+
+Las APIs de terceros |
+    no estan en el navegador de forma predeterminada y, por lo general, debes obtener un codigo e informacion de algun lugar de la Web.
+        API de Twitter |
+            permite hacer cosas como mostrar tus ultimos tweets en tu web.
+        API de Google Maps |
+            insertar mapas personalizados en tu web y otras funciones similares.
+
 */
+
