fdded695944b1c3fa4e3966faef32d624ee3a817
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-18 17:38:07 -0300

    mdn documentation | API interfaces

---
+++ mozilla-mdn/que-es-javascript.js
@@ -0,0 +1,6 @@
+/* API  interface de programacion de aplicaciones (API)
+
+Las API son conjuntos de bloques de construccion de codigo listos para usar quq permiten a un desarrollador implementar programas que ded otro modo serian dificiles o imposibles dde implementar.
+Hacen lo mismo para lo programacion que lso kits de muebles prefabricados para la construccion de viviendas --- es mucho mas facil tomar paneles precortados y atornillarlos para hacer una estntaria que elaborar el disegnio tu mismo,  que ir y encontrar la madera correcta, cortar todos los paneles del tamagnio y la forma correctos, buscar los tornillo del tamagnio correcto y luego juntalos para hacer una estanteria.
+
+*/
