d06431bb83273e450794c35937f14753f7af0e13
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-19 04:51:06 -0300

    mdn documentation | adivinar numero script

--- mozilla-mdn/juego-adivinar-numero/script.js
+++ mozilla-mdn/juego-adivinar-numero/script.js
@@ -1,1 +1,13 @@
 console.log('se cargo exitosamente !');
+
+/* 
+<div class="container">
+    <input type="text" class="inputForm">
+    <button class="btn">Enviar respuesta</button>
+    <p class="intentosAnteriores"></p>
+</div>
+*/
+
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', () => console.log('button clicked !!'))
