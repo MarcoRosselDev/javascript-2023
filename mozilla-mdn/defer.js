37bac1369d229abb51216ded41a4b8fbd8902c3c
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-18 21:57:57 -0300

    mdn documentation | JavaScript defer

---
+++ mozilla-mdn/defer.js
@@ -0,0 +1,9 @@
+/* 
+    <script src="./script.js" defer></script>
+
+    defer nos soluciona el problema de espera del cargado del DOM
+    ya no es nesesario evitare errores colocando el script al final del documento ,
+    justo arriba del cierre de body.
+    aplicar script en el header nos ayuda en el rendimiento, ahora puede cargar al mismo tiempo que el DOM.
+    ajilizando todo el proseso en especial si la web es grande.
+*/
