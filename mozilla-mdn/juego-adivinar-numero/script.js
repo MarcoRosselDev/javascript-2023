bd177485f24984ab7666a8e379ed69161e44f6df
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-21 04:59:30 -0300

    mdn documentation | adivinar numero script 5

--- mozilla-mdn/juego-adivinar-numero/script.js
+++ mozilla-mdn/juego-adivinar-numero/script.js
@@ -5,12 +5,14 @@ console.log('se cargo exitosamente !');
     <input type="text" class="inputForm">
     <button class="btn">Enviar respuesta</button>
     <p class="intentosAnteriores"></p>
+    <p class="mensajeIntentos"></p>
 </div>
 */
 
 const inputForm = document.querySelector('.inputForm')
 const btn = document.querySelector('.btn')
 const paraf = document.querySelector('.intentosAnteriores')
+const mensajeIntentos = document.querySelector('.mensajeIntentos')
 
 let numeros = []
 let intentos = 1
@@ -23,14 +25,18 @@ btn.addEventListener('click', function () {
     console.log(typeof inputForm.value);
 
     numeros.push(inputForm.value)
+
+
     if (inputForm.value == randomNumber) {
         paraf.textContent = `Ganaste !! el numero es ${randomNumber}`
+        mensajeIntentos.textContent = ''
         // agregar boton para jugar denuevo.
     } else {        
-        paraf.textContent = `segue intentando, te quedan n intentos`
+        paraf.textContent = numeros
+        mensajeIntentos.textContent = `segue intentando, te quedan n intentos`
         // imprimir en panatalla los numero anteriores
     }
-    
+    inputForm.value = ''
 })
 
 
