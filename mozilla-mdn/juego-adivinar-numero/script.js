b2778d9bde7b3bedd8198e36072dd0183bc52a55
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-21 05:06:27 -0300

    mdn documentation | adivinar numero script 7

--- mozilla-mdn/juego-adivinar-numero/script.js
+++ mozilla-mdn/juego-adivinar-numero/script.js
@@ -4,39 +4,44 @@ console.log('se cargo exitosamente !');
 <div class="container">
     <input type="text" class="inputForm">
     <button class="btn">Enviar respuesta</button>
+    <p class="pista"></p>
     <p class="intentosAnteriores"></p>
     <p class="mensajeIntentos"></p>
 </div>
 */
 
-const inputForm = document.querySelector('.inputForm')
-const btn = document.querySelector('.btn')
-const paraf = document.querySelector('.intentosAnteriores')
-const mensajeIntentos = document.querySelector('.mensajeIntentos')
+const inputForm = document.querySelector('.inputForm');
+const btn = document.querySelector('.btn');
+const paraf = document.querySelector('.intentosAnteriores');
+const mensajeIntentos = document.querySelector('.mensajeIntentos');
+const pista = document.querySelector('.pista');
 
-let numeros = []
-let intentos = 1
-let randomNumber = Math.floor(Math.random() * 100) + 1
+let numeros = [];
+let intentos = 1;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 console.log(randomNumber);
 
 btn.addEventListener('click', function () {
-
-    console.log(typeof inputForm.value);
-
     numeros.push(inputForm.value)
-
 
     if (inputForm.value == randomNumber) {
         paraf.textContent = `Ganaste !! el numero es ${randomNumber}`
         mensajeIntentos.textContent = ''
+        pista.textContent = ''
         // agregar boton para jugar denuevo.
     } else {        
+        // pista
+        if (inputForm.value > randomNumber) {
+            pista.textContent = 'El numero es demaciado grand.'
+        } else {
+            pista.textContent = 'El numero es demaciado pequegnio.'
+        }
+        // 
         paraf.textContent = numeros
         mensajeIntentos.textContent = `segue intentando, te quedan n intentos`
-        // imprimir en panatalla los numero anteriores
     }
     inputForm.value = ''
-})
+});
 
 
