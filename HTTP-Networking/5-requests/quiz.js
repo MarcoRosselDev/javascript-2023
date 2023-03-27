e347ef6763b5b907d07153566937c73137b77ed0
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-27 13:04:43 -0300

    5 request

---
+++ HTTP-Networking/5-requests/quiz.js
@@ -0,0 +1,16 @@
+const itemURL = 'https://api.boot.dev/v1/courses_rest_api/learn-http/items'
+
+const items =  getData(itemURL)
+console.log(items)
+
+async function getData(url) {
+    const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'X-API-Key': 'Testing',
+            'Content-Type': 'application/json'
+        }
+    })
+    return response.json()
+}
