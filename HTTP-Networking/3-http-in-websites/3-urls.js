b3ec7231947119b32905cabc027f908f8d0967a5
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-27 13:01:14 -0300

    3 urls | code

--- HTTP-Networking/3-http-in-websites/3-urls.js
+++ HTTP-Networking/3-http-in-websites/3-urls.js
@@ -3,6 +3,7 @@
 una URL o Uniform Resource Locator (Localizador Uniforme de Recursos) es esencialmente la direccion de otra computadora,
 o servido en internet.
 Parte de la URL espcifica como llegar al servidor,  y parte le dice al servidor que informacion queremos.
+
 Por ahora nos importa saber que la URL representa una piesa de informacion en otra computadora a la que queremos acceder.
 nosotros podemos tener acceso a ella con una Request, y leyendo la respuesta con la que responde el servidor.
 
