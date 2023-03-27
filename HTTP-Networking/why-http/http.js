958b6daee727a89502209dc642e4059a3c83bb03
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-27 07:58:14 -0300

    why http

---
+++ HTTP-Networking/why-http/http.js
@@ -0,0 +1,16 @@
+/* Protocolo de transferencia de hipertexto (HTTP) ===  'hypertext transfer protocol'. 
+
+
+
+*/
+
+/* comunicacion en la web
+
+instagram seria vastante horrible si copiaras y enviaras manualmente tus fotos a tus amigos cuando quiesieres ense;arselas.
+las apps modernas nesecitan comunicar informacion entre dispositivos atraves de internet.
+
+    Gmail no solo almacena tus mails solo en variables en tu computadora. el los almacena en un computador en centros de datos.
+    Tu no pierdes todos tus mensajes si pierdes tu computador en un lago, todos esos mensajes existen en  slack's servers.
+
+
+*/
