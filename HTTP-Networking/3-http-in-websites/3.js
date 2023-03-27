1de43e70c8514c7949975241eb629bae9c5a5d3d
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-27 11:37:35 -0300

    http in websites

---
+++ HTTP-Networking/3-http-in-websites/3.js
@@ -0,0 +1,20 @@
+/* HTTP powers websites     http potencia sitios web
+
+hay otros protocolos de comunicacion atraves de internet, pero HTTP es el mas popular
+y es particularmente bueno para sitios web y apps.
+cada vez que tu visitas una web, tu navegador 'chrom or mozilla' esta solicitando una HTTP Request al 
+servidor web.
+el servidor responde con todo el texto, imagenes y estilos que el navegador nesecita para 
+renderisar la pagina web.
+
+pregunta:
+    website data is requested via Which internet communication protodol?
+
+a   "HTTP",
+b   "UDP",
+c   "RabbitMQ",
+d   "PSQL"  
+
+respuesta = a
+
+*/
