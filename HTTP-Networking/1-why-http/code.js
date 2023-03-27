901bb41d3faac3697727c8ce61ef3c35ecdfd67c
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-27 08:26:51 -0300

    why http | code  async

--- HTTP-Networking/1-why-http/code.js
+++ HTTP-Networking/1-why-http/code.js
@@ -1,7 +1,21 @@
+const items = await getItemData()
 const apiKey = generateKey()
 
+logItems(items)
 
-function generateKey () {
+async function getItemData(){
+    const response = await fetch('https://api.boot.dev/v1/courses_rest_api/learn-http/items', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'X-API-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    })
+    return response.json()
+}
+
+function generateKey(){
     const characters = 'ABCDEF0123456789'
     let result = ''
     for (let i = 0; i < 16; i++) {
@@ -10,3 +24,8 @@ function generateKey () {
     return result
 }
 
+function logItems(items) {
+    for(const item of items){
+        console.log(item.name);
+    }
+}
