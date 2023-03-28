7017dc01f6ade4794ef01a58c106e9941048ff0a
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-28 16:25:29 -0300

    web addresses | IP 1

--- HTTP-Networking/2-DNS/1-addresses/web-addresses.js
+++ HTTP-Networking/2-DNS/1-addresses/web-addresses.js
@@ -6,8 +6,8 @@ web clients find other computers over the internet using Internet Protocol or IP
 
 An IP address is a numerical label that serves two main functions:
 
-Location Addressing
-Network Identification
+1-  Location Addressing
+2-  Network Identification
 ------------------------------------------------------------------------------------------------------
 spa
 
@@ -17,6 +17,35 @@ otras computadoras atraves de internet usando Internet Protocol o IP addresses.
 
 una IP adress es una etiqueta numerica que cumple dos funciones principales.
 
-Direccionamiento de Ubicacion
-Identificacion de Red
+1-  Direccionamiento de Ubicacion
+2-  Identificacion de Red
+*/
+
+/* Domain names and IP addresses    |   Nombres de dominio y direcciones IP 
+eng
+
+Each device connected to the internet has a unique IP address. When we browse the internet, 
+the domains we navigate to are all associated with a particular IP address!
+
+For example, this Wikipedia URL points to a page about miniature pigs: 
+https://en.wikipedia.org/wiki/Miniature_pig
+
+The domain portion of the URL is en.wikipedia.org.
+
+en.wikipedia.org converts to a specific IP address, 
+and that IP address tells your computer exactly where to communicate with that Wikipedia page.
+------------------------------------------------------------------------------------------------------
+spa
+
+cada dispositivo conectado a internet tiene una direccion IP unica.
+cuando navegamos por internet, los dominios a lso que navegamos estan todos asociados con IP particulares!
+
+por ejemplo, esta URL de wikipedia apunta a una pagina sobre serdos peque;os.
+https://en.wikipedia.org/wiki/Miniature_pig
+
+la parte del dominio de la URL es en.wikipedia.org
+
+en.wikipedia.org se convierte en una direccion IP especifica,
+y esa direccion IP le dice a su computadora exactamente donde comunicarse con esa pagina de wikipedia.
+
 */
