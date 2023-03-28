8f7026733980db8249c28b20d87c8e526f8a1af0
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-28 14:30:40 -0300

    web addresses | IP

--- HTTP-Networking/2-DNS/1-addresses/web-addresses.js
+++ HTTP-Networking/2-DNS/1-addresses/web-addresses.js
@@ -1,1 +1,22 @@
-/* Web Addresses */
+/* Web Addresses
+eng
+In the real world, we use addresses to help us find where a friend lives, 
+where a business is located, or where a party is being thrown. In computing, 
+web clients find other computers over the internet using Internet Protocol or IP addresses.
+
+An IP address is a numerical label that serves two main functions:
+
+Location Addressing
+Network Identification
+------------------------------------------------------------------------------------------------------
+spa
+
+en el mundo real, nosotros usamos direcciones para ayudarnos a encontrar dodnde vive un amigo,
+donde se ubica un negocio, o donde sera la fiesta. en computacion, los clientes web encuentran 
+otras computadoras atraves de internet usando Internet Protocol o IP addresses.
+
+una IP adress es una etiqueta numerica que cumple dos funciones principales.
+
+Direccionamiento de Ubicacion
+Identificacion de Red
+*/
