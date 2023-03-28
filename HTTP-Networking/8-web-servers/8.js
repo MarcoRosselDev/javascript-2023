c80a984819514293f9c7e375b1b8dddff00073b1
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-28 14:16:21 -0300

    8 web servers b

--- HTTP-Networking/8-web-servers/8.js
+++ HTTP-Networking/8-web-servers/8.js
@@ -10,10 +10,49 @@ datos para que permanezcan por mas de una sola sesion y puedan ser accedido por 
 */
 
 /* Listening and serving data   |   escuchando y sirviendo datos
+eng
+
+Similar to how a server at a restaurant brings your food to the table, a web server serves web resources, 
+such as web pages, images, and other data. The server is turned on and "listening"
+for inbound requests constantly so that the second it receives a new request,
+it can send an appropriate response.
+--------------------------------------------------------------------------------------------------------
+esp
 
 similar a como le sirven en un restoran su comida, sua web server sirbe recursos webs, como una pagina web,
 imagenes y otros datos.
 El servidor esta encendido y "escuchando" las solicitudes entrantes constantemente para que,
 en el momento en que reciba una nueva solicitud, pueda enviar una respuesta adecuada.
+
+*/
+
+/* The server is the back-end   |   El servidor es el back-end
+eng
+
+While the "front-end" of a website or web application is the device the user interacts with, 
+the "back-end" is the server that keeps all the data housed in a central location.
+--------------------------------------------------------------------------------------------------------
+spa
+
+Mientras que el "front-end" de un sitio web o una aplicación web es el dispositivo con el que interactúa el usuario,
+el "back-end" es el servidor que mantiene todos los datos alojados en una ubicación central.
+
+*/
+
+/* A server is just a computer  |   Un servidor es solo una computadora
+eng
+
+"Server" is just the name we give to a computer that is taking on the role of serving
+data across a network connection. A good server is turned on and available 24 hours a day,
+7 days a week. While your laptop can be used as a server, it makes more sense to use a computer 
+in a data center that's designed to be up and running constantly.
+--------------------------------------------------------------------------------------------------------
+spa
+
+"Servidor" es solo el nombre que le damos a una computadora que asume la función de servir datos 
+a través de una conexión de red. 
+Un buen servidor está encendido y disponible las 24 horas del día, los 7 días de la semana. 
+Si bien su computadora portátil se puede usar como servidor, tiene más sentido usar una computadora 
+en un centro de datos que está diseñado para estar en funcionamiento constantemente.
 
 */
