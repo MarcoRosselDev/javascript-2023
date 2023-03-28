4017f29e6ec1a0b14d9561cdfce65aad8150b62d
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-28 08:56:45 -0300

    5a request

---
+++ HTTP-Networking/5a-request-responses/5a.js
@@ -0,0 +1,28 @@
+/*  -un "client" es una computadora haciendo una HTTP request
+    -un "server" es una computadora respondiendo a una HTTP request
+    -un computador puede ser un client, un server, los dos, o ninguno.
+        "client" and "server" son solo palabras que usamos para describir que es lo que
+        hace un computador en una comunicacion de systemas.
+    -"clients" envian requests y reciven "responses"
+    -"server" reciven requests y envian "responses"
+
+*/
+
+// codigo de ejemplo
+const itemURL = 'https://api.boot.dev/v1/courses_rest_api/learn-http/items'
+
+const items = await getData(itemURL)
+
+console.log(items)
+
+async function getData(url) {
+    const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+        'X-API-Key': 'Testing',
+        'Content-Type': 'application/json'
+        }
+    })
+    return response.json()
+}
