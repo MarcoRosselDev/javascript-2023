25a243e9033c4266263d3122407bd4c2ec0277d3
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-28 13:53:45 -0300

    8 web servers

---
+++ HTTP-Networking/7-web-clients/7.js
@@ -0,0 +1,15 @@
+/* Web Clients
+
+    - como vimos, un web client es un dispositivo solicitando una request al web server.
+    - un "client" puede ser cualquier tipo de dispositiovo pero es siempre alguien interactuando fisicament.
+        por ejemplo:
+            un computador de escritorio
+            un celular
+            una tablet
+        
+    en un sitio web o aplicacion web, llamamos a los usuarios dispositivos del "front-end" (en frente).
+
+    un Front-end client solicita requests al back-end server.
+
+
+*/
---
+++ HTTP-Networking/8-web-servers/8.js
@@ -0,0 +1,19 @@
+/* Web Servers
+
+hasta este punto, la mayoria de los datos con los que ha trabajado en su codigo simplemente se generaron 
+y se almacenaron localmente en variable.
+
+si bien siempre usara variables para almacenar y manipular datos mientras se ejecuta su programa,
+la mayoria de los sitios web y las aplicaciones usan un servidor web para almacenar, ordenar y servir esos 
+datos para que permanezcan por mas de una sola sesion y puedan ser accedido por multiples despositivos.
+
+*/
+
+/* Listening and serving data   |   escuchando y sirviendo datos
+
+similar a como le sirven en un restoran su comida, sua web server sirbe recursos webs, como una pagina web,
+imagenes y otros datos.
+El servidor esta encendido y "escuchando" las solicitudes entrantes constantemente para que,
+en el momento en que reciba una nueva solicitud, pueda enviar una respuesta adecuada.
+
+*/
