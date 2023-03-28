e4c0f526c804919b5b40bd67327d61b9f21532c0
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-28 18:11:11 -0300

    web addresses | brak

---
+++ HTTP-Networking/2-DNS/4-dns-quiz/domain-name-system.js
@@ -0,0 +1,16 @@
+/* What is the Domain Name System ?
+
+eng
+
+So we've talked about domain names and what their purpose is, but we haven't talked about the system that's used to do that conversion.
+
+DNS, or the "Domain Name System", is the phonebook of the internet. Humans connect to websites through domain names, 
+like Boot.dev. DNS "resolves" these domain names to find the associated IP addresses so that web clients can load the resources for the specific address.
+-----------------------------------------------------------------------------------------------------------------------------------------
+spa
+
+hablamos sobre el nombre de dominio y cual es su proposito, pero no hemos hablado sobre el sistema que se usa para hacer esa conversion.
+
+
+
+*/
