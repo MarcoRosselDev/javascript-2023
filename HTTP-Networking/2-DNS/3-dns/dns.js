62cac22795e0732eaae01d43617257237aa1a66e
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-28 17:33:59 -0300

    web addresses | dns code successful

--- HTTP-Networking/2-DNS/3-dns/dns.js
+++ HTTP-Networking/2-DNS/3-dns/dns.js
@@ -13,3 +13,28 @@ por ejemplo, la URL https://example.com/path es un hostname de example.com.
 la parte https:// y /path no forman parte de el nombre de dominio --> direccion IP mapea eso,  vamos a aprender sobre eso. 
 
 */
+
+/* Using the URL in javascript
+
+la URL API es construida en javaScript. tu puedes crear un nuevo objeto URL;
+    
+    const urlObj = new URL('https://example.com/example-path')
+
+y entonces tu puedes extraer solo el nombre de dominio:
+
+    const urlObj.hostname
+
+*/
+
+function getDomainNameFromURL(url) {
+    const urlObj = new URL(url);
+    return urlObj.hostname
+}
+
+// The domain name for https://boot.dev/learn/learn-python is boot.dev
+    
+// don't touch below this line
+    
+const bootdevURL = 'https://boot.dev/learn/learn-python'
+const domainName = getDomainNameFromURL(bootdevURL)
+console.log(`The domain name for ${bootdevURL} is ${domainName}`)
