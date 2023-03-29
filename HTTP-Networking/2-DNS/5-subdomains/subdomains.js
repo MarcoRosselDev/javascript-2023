9bfc54e9bd3594b98cd5f372ce3a2b97bed3ec31
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 13:46:05 -0300

    web addresses | subdominio

---
+++ HTTP-Networking/2-DNS/5-subdomains/subdomains.js
@@ -0,0 +1,32 @@
+/* Subdomains
+
+eng
+
+We learned about how a domain name resolves to an IP address, 
+which is just a computer on a network - often the internet.
+
+A subdomain prefixes a domain name, 
+allowing a domain to route network traffic to many different 
+servers and resources.
+
+For example, the Boot.dev website that you're on right now is 
+hosted on a different computer than our blog. Our blog, 
+found at blog.boot.dev is hosted on our "blog" subdomain.
+
+--------------------------------------------------------------
+
+spa
+
+Aprendimos sobre como el nombre de dominio soluciona una direccion IP,
+lo que es solo una computadora en una red, a menudo internet.
+
+Un subdominio precede aun nombre de dominio,
+permite que un dominio enrute el trafico de red a muchos 
+servidores y recursos. 
+
+por ejemplo, el sitio Boot.dev en el que estas ahora mismo es
+alojado en un computador diferente a la de nuestro blog. Nuestro blog,
+se encuentra en blog.boot.dev esta alojado en nuestro subdominio "blog". 
+
+
+*/
