34c0b8b507e5e609c117f25efd0c72fdc52798c8
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 13:24:02 -0300

    web addresses | como trabaja

---
+++ HTTP-Networking/2-DNS/4-dns-quiz/como-trabaja-DNS.js
@@ -0,0 +1,40 @@
+/*  How does DNS Works ?    |   Como trabaja DNS ?
+
+eng
+
+We'll go into more detail on DNS in a future course, 
+but to give you a simplified idea of how it works, 
+let's introduce ICANN. 
+ICANN is a not-for-profit organization 
+that manages DNS for the entire internet.
+
+Whenever your computer attempts to resolve a domain name, 
+it contacts one of ICANN's "root nameservers" whose address 
+is included in your computer's networking configuration. 
+From there, that nameserver can gather the domain records 
+for a specific domain name from their distributed DNS database.
+
+If you think of DNS as a phonebook, ICANN is the publisher 
+that keeps the phonebook in print and available.
+
+----------------------------------------------------------------
+
+spa
+
+Entraremoa en mas detalles sobre el DNS en el futuro, 
+pero para darte una idea simple de como trabaja,
+vamos a precentarte ICANN.
+ICANN es una organizacion sin fines de lucro que administra 
+DNS para todo internet.
+
+cada vez que tu computador intenta resolver un nombre de dominio,
+se contacta con uno de los "servidores de nombres raiz" de ICANN
+cuya direccion es incluida en tu configuracion de red de computadora.
+A partir de ahi, ese servidor de nombres puede recopilar los registros 
+de dominio para un nombre de dominio especifico de su base de 
+datos DNS distribuida.
+
+si piensas que DNS es una guia telefonica, ICANN es el editor
+que mantiene la guia telefonica impresa y disponible.
+
+*/
