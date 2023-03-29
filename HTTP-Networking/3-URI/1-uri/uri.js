06a89d40a13b5c085438c64dda19273b4c5c54a8
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 15:22:21 -0300

    uri

---
+++ HTTP-Networking/3-URI/1-uri/uri.js
@@ -0,0 +1,43 @@
+/* Uniform Resource Identifiers |   Identificadores Uniformes de Recursos
+
+eng
+
+We briefly touched on URLs earlier, 
+let's dive a little deeper into the subject.
+
+A URI, or Uniform Resource Identifier, is a unique character sequence that 
+identifies a resource that is (almost always) accessed via the internet.
+
+Just like JavaScript has syntax rules, so do URIs. 
+These rules help ensure uniformity so that any program can 
+interpret the meaning of the URI in the same way.
+
+URIs come in two main types:
+    URLs
+    URNs
+
+We will focus specifically on URLs in this course, 
+but it's important to know that URLs are only one kind of URI.
+
+--------------------------------------------------------------------------------
+
+spa
+
+Anteriormente mencionamos brevemente las URL, 
+profundicemos un poco mas en el tema.
+
+un URI, o identificador uniforme de recursos, es una secuencia de caracteres
+unica que identifica un recurso al que (casi siempre) se accede a traves de internet.
+
+Al igual que javascript tiene reglas de sintaxis, tambien las tiene las URI.
+Estas reglas ayudan a garantizar la uniformidad para que cualquier 
+programa pueda interpretar el significado de la URI de la misma manera.
+
+las URI vienen en dos tipos principales:
+    URLs
+    URNs
+
+vamos a enfocarnos en las URLs en este curso,
+pero es importante para saber que esa URLs es solo una clase de URI
+
+*/
