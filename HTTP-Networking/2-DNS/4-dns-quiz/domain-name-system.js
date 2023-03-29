7906fdb66541e074680b94eea54624f97b3a30ba
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 13:10:34 -0300

    web addresses | b

--- HTTP-Networking/2-DNS/4-dns-quiz/domain-name-system.js
+++ HTTP-Networking/2-DNS/4-dns-quiz/domain-name-system.js
@@ -2,15 +2,24 @@
 
 eng
 
-So we've talked about domain names and what their purpose is, but we haven't talked about the system that's used to do that conversion.
+So we've talked about domain names and what their purpose is, 
+but we haven't talked about the system that's used to do that conversion.
 
-DNS, or the "Domain Name System", is the phonebook of the internet. Humans connect to websites through domain names, 
-like Boot.dev. DNS "resolves" these domain names to find the associated IP addresses so that web clients can load the resources for the specific address.
------------------------------------------------------------------------------------------------------------------------------------------
+DNS, or the "Domain Name System", is the phonebook of the internet. 
+Humans connect to websites through domain names, 
+like Boot.dev. 
+DNS "resolves" these domain names to find the associated 
+IP addresses so that web clients can load the resources for the specific address.
+--------------------------------------------------------------------
 spa
 
-hablamos sobre el nombre de dominio y cual es su proposito, pero no hemos hablado sobre el sistema que se usa para hacer esa conversion.
+hablamos sobre el nombre de dominio y cual es su proposito, 
+pero no hemos hablado sobre el sistema que se usa para hacer esa conversion.
 
-
+DNS, o el "Sistema de nombres de dominio", es la guia telefonica de internet.
+los humanos se conectan a sitios web a traves de los nombres de dominio.
+como Boot.dev.  
+DNS, "resuelve" el nombre de dominio para encontrar la direccion IP asociada,
+entonces el cliente web puede cargar el requerimiento para esa direccion especifica.
 
 */
