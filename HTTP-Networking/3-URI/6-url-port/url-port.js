d03e22582ee175aa8bf0e600dbcd46b783277fad
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 22:58:51 -0300

    uri | puertos

---
+++ HTTP-Networking/3-URI/6-url-port/url-port.js
@@ -0,0 +1,26 @@
+/* URL Ports
+eng
+
+The port in a URL is a virtual point where network connections are made. 
+Ports are managed by a computer's operating system '
+and are numbered from 0 to 65,535.
+
+Whenever you connect to another computer over a network, 
+you're connecting to a specific port on that computer, 
+which is being listened to by a specific piece of software on that computer. 
+A port can only be used by one program at a time, 
+which is why there are so many possible ports.
+------------------------------------------------------------------------------
+spa
+
+el puerto en una URL es un apuntador virtual donde se realizan las conexiones de red.
+Los puertos son manejadores por el sistema operativo de una computadora
+y estan numerados del 0 al 65,535.
+
+Cada vez que se conecta a otra computadora a travaes de una red,
+te estaas conectando a un puerto especifico en esa computadora,
+que esta siendo escuchado por una pieza especifica de software en esa computadora.
+un puerto solo puede ser utilizado por un programa a la vez, 
+por eso hay tantos puertos posibles.
+
+*/
