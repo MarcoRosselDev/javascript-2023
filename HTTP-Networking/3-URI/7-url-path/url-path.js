e6b30e839507b69c049a9bf609317c8541d38852
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 23:12:29 -0300

    uri | path

---
+++ HTTP-Networking/3-URI/7-url-path/url-path.js
@@ -0,0 +1,34 @@
+/* URL Paths
+eng
+
+In the early days of the internet, 
+a URL's path often was a reflection of the file path on the server 
+to the resource the client was requesting.
+
+For example:
+if the website https://exampleblog.com had a web server running within its /home directory, 
+then a request to the https://exampleblog.com/site/index.html URL might expect the index.html file from 
+within the /home/site directory to be returned.
+
+Websites used to be very simple. 
+They were just a collection of text documents stored on a server. 
+A simple piece of server software could handle incoming HTTP requests and respond 
+with the documents according to the path component of the URLs.
+----------------------------------------------------------------------------------------
+spa
+
+En los primeros dias de internet, 
+la ruta de una URL a menudo reflejaba la ruta del archivo en el servidor
+al recurso que solicitaba el cliente.
+
+Por ejemplo:
+si el sitio web https://exampleblog.com tenia un servidor web ejecutandose dentro de su directorio /home
+entonces una solicitud a la URL https://exampleblog.com/site/index.html podria esperar el archivo index.html
+desde dentro del directorio  /home/site para ser devuelto.
+
+los sitios web solian ser muy simples.
+Eran solo una coleccion de documentos de texto almacenados en un servidor.
+Una simple pieza de software de servidor podria manejar las solicitudes HTTP entrantes
+y responder con los documentos de acuerdo con el componente de ruta de las URL.
+
+*/
