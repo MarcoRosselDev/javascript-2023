be25613cffd2b6b8f110809500f69f03c3ccf936
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 23:03:46 -0300

    uri | puertos | b

--- HTTP-Networking/3-URI/6-url-port/url-port.js
+++ HTTP-Networking/3-URI/6-url-port/url-port.js
@@ -10,6 +10,15 @@ you're connecting to a specific port on that computer,
 which is being listened to by a specific piece of software on that computer. 
 A port can only be used by one program at a time, 
 which is why there are so many possible ports.
+
+The port component of a URL is often not visible when browsing normal sites on the internet, 
+because 99% of the time you're using the default ports for the HTTP and HTTPS schemes: 
+    80 and 443 respectively.
+
+Whenever you aren't using a default port, you need to specify it in the URL. 
+For example, 
+port 8080 is often used by web developers when they're running their server in "test mode" 
+so that they don't use the "production" port "80".
 ------------------------------------------------------------------------------
 spa
 
@@ -22,5 +31,15 @@ te estaas conectando a un puerto especifico en esa computadora,
 que esta siendo escuchado por una pieza especifica de software en esa computadora.
 un puerto solo puede ser utilizado por un programa a la vez, 
 por eso hay tantos puertos posibles.
+
+El componente de puerto de una URL a menudo no es visible cuando navega por sitios normales en internet, 
+porque el 99% del tiempo esta usando los puertos predeterminados para los esquemas HTTP y HTTPS:
+    80 y 443 respectivamente.
+
+siempre que no utilice un puerto predeterminado, debe especificarlo en la URL.
+por ejemplo.
+Los desarrolladores web suelen utilizar el puerto 8080 cuando ejecutan su servidor en "modo de prueba"
+para no utlizar el puerto "80" de "produccion".
+
 
 */
