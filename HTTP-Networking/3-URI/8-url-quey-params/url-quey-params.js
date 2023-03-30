e1564f1bd77c22f5569f07c11c90a7bc2893cbbb
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-30 13:01:54 -0300

    uri | path | query

---
+++ HTTP-Networking/3-URI/8-url-quey-params/url-quey-params.js
@@ -0,0 +1,24 @@
+/* Query parameters
+eng
+
+Query parameters in a URL are not always present. 
+In the context of websites, query parameters are often used 
+for marketing analytics or for changing a variable on the web page. 
+With website URLs, query parameters rarely change which page you're viewing, 
+though they often will change the page's contents.
+
+That said, query parameters can be used for anything the server chooses to use them for, 
+just like the URL's path.
+--------------------------------------------------------------------------------
+spa
+
+Los parametros de consulta en una URL no siempre estan presentes.
+En el contexto de los sitio web, los parametros de consulta a menudo 
+son usados para analisis de marketing o para cambiar una variable en la pagina web.
+Con las URL de los sition web, los parametros de consulta rara vez cambian la pagina que esta viendo,
+aunque a menudo cambiaran el contenido de la pagina.
+
+Dicho esto, los parametros de consulta pueden ser usados para cualquier cosa que el servidor decida usar,
+al igual que la ruta de la URL.
+
+*/
