de2c4018ccdfdbe40a87b71f49ab84e71e1a3f87
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-30 14:33:15 -0300

    async | asunchronous | example

--- HTTP-Networking/4-async/1-async/async.js
+++ HTTP-Networking/4-async/1-async/async.js
@@ -12,6 +12,15 @@ Example of synchronous code:
 Asynchronous or async code runs in parallel. 
 That means code further down runs at the same time that a previous line of code is still running. 
 A good way to visualize this is with the JavaScript function setTimeout().
+
+setTimeout accepts a function and a number of milliseconds as inputs. 
+It waits until the number of milliseconds has elapsed, and then it executes the function it was given.
+
+Example of asynchronous code:
+
+console.log("I print first");
+setTimeout(() => console.log("I print third because I'm waiting 100 milliseconds"), 100)
+console.log("I print second");
 ----------------------------------------------------------------
 spa
 
@@ -27,5 +36,26 @@ Ejemplo de codigo sincrono :
 El codigo sincrono o asincrono se ejecuta en paralelo.
 Eso significa que el codigo mas abajo se ejecuta al mismo tiempo que se sigue ejecutando una lina de codigo anterior.
 Una buena forma de visualizar esto es con la funcion de javaScript setTimeout()
+
+setTimeout acepta una funcion y un numero en milisegundo como entrada.
+Espera hasta que haya transcurrido la cantidad de milisegundos y luego ejecuta la funcion que se le dio.
+
+Ejemplo de codigo asincrono:
+
+console.log("I print first");
+setTimeout(() => console.log("I print third because I'm waiting 100 milliseconds"), 100)
+console.log("I print second");
+
+*/
+
+console.log("I print first");
+setTimeout(() => console.log("I print third because I'm waiting 100 milliseconds"), 100)
+console.log("I print second");
+
+/* log
+
+I print first
+I print second
+I print third because I'm waiting 100 milliseconds
 
 */
