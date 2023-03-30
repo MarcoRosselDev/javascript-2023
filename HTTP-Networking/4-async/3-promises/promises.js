92775e5c24c05d5d8f2d36e0b1d0825343c78937
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-30 16:14:14 -0300

    async | asunchronous | promesas

---
+++ HTTP-Networking/4-async/3-promises/promises.js
@@ -0,0 +1,33 @@
+/* Promises in JavaScript 
+eng
+
+A Promise in JavaScript is very similar to making a promise in the real world. 
+When we make a promise we are making a commitment to something. 
+For example, I promise to explain JavaScript promises to you, 
+my promise to you has 2 potential outcomes: 
+    it is either fulfilled, meaning I eventually explained promises to you, 
+    or it is rejected meaning I failed to keep my promise.
+
+The Promise Object represents the eventual fulfillment or 
+rejection of our promise and holds the resulting values. 
+In the meantime, while we're waiting for the promise to be fulfilled, 
+our code continues executing. 
+Promises are the most popular modern way to write asynchronous code in JavaScript.
+
+----------------------------------------------------------------------
+spa
+
+Una promesa en JavaScript es similar a hacer una promesa en la vida real.
+Cuando hacemos una promesa nos estamos comprometiendo con algo.
+Por ejemplo, yo prometo explicarte las promesas de JavaScript,
+mi promesa tiene 2 posibles resultados:
+    Se cumple, lo que significa que finalmente le explique las promesas
+    Se rechaza, lo que significa que no cumpli mi promesa.
+
+El objetivo de la promesa representa el eventual cumplimiento o 
+rechazo de nuestra promesa y contiene los valores resultantes.
+Mientras tanto, mientras esperamos que se cumpla la promesa, 
+nuestro codigo continua ejecutandose.
+Las promesas son la forma moderna mas popular de escribir codigo asincrono en JavaScript.
+
+*/
