571f052025d7fd1fec53acb23ac0cf775104c5b7
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 23:18:20 -0300

    uri | path | now

---
+++ HTTP-Networking/3-URI/7-url-path/url-path-presente.js
@@ -0,0 +1,24 @@
+/* These days, it's not always about the filesystem
+eng
+
+On many modern web servers, 
+a URL's path isn't a reflection of the server's filesystem hierarchy. 
+Paths in URLs are essentially just another type of parameter 
+that can be passed to the server when making a request.
+
+Conventionally, two different URL paths should denote different resources. 
+For example, different pages on a website, 
+or maybe different data types from a game server.
+-------------------------------------------------------------------------------------
+spa
+
+En muchos servidores web modernos, 
+la ruta de una URL no refleja la jerarquia del sistema de archivos del servidor.
+Las rutas en las URL son esencialemente otro tipo de parametro 
+que se puede pasar al servidor al realizar una solicitud.
+
+Convencionalmente, dos rutas de URL diferentes deberian indicar diferentes recursos.
+Por ejemplo, diferentes paginas en un sitio web, 
+o tal vez diferentes tipos de datos de un servidor de juegos. 
+
+*/
