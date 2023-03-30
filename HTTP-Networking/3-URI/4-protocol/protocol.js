d782675888b25ae88245a718ed2190d16d5307d7
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 21:38:10 -0300

    uri | protocolo

---
+++ HTTP-Networking/3-URI/4-protocol/protocol.js
@@ -0,0 +1,42 @@
+/* The Protocol     
+eng
+
+The "protocol", also referred to as the "scheme", 
+is the first component of a URL. 
+Its purpose is to define the rules by which the data 
+being communicated is displayed, encoded or formatted.
+
+Some examples of different URL protocols:
+
+-   http
+-   ftp
+-   mailto
+-   https
+
+For example:
+
+-   http://example.com
+-   mailto:noreply@fantasyquest.app
+
+---------------------------------------------------------------------------
+spa
+
+El "protocolo", tambien denominado "esquema", 
+es el primer componente de una URL.
+Su proposito es definir las reglas por las cuales los datos
+que se comunican se muestran, codifican o formatean.
+
+Algunos ejemplos de diferentes protocolos de URL:
+
+-   http
+-   ftp
+-   mailto
+-   https
+
+por ejemplo:
+
+-   http://example.com
+-   mailto:noreplay@fantasyquest.app
+
+
+*/
