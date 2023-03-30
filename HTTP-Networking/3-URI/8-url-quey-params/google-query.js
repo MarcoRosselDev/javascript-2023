186b6b2435c1105313b9517b2d3c48d97b90dfbb
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-30 13:11:19 -0300

    uri | path | query google

---
+++ HTTP-Networking/3-URI/8-url-quey-params/google-query.js
@@ -0,0 +1,29 @@
+/* How Google uses query parameters
+eng
+
+1   Open a new tab and go to https://google.com.
+2   Search for "hello world"
+3   Take a look at your current URL. It should start with https://www.google.com/search?q=hello+world
+4   Change the URL to say https://www.google.com/search?q=hello+universe
+5   Press "enter"
+
+You should see new search results for the query "hello universe". 
+Google chose to use query parameters to represent the value of your search query. 
+It makes sense - each search result page is essentially 
+the same page as far as structure and formatting are concerned -
+    it's just showing you different results based on the search query.
+------------------------------------------------------------------------
+spa
+
+1   Abra una nueva pesta;a y vaya a https://google.com.
+2   Buscar "hola mundo"
+3   Echa un vistazo a su URL actual. Debe comenzar con https://www.google.com/search?q=hello+wold
+4   Cambia la URL para decir https://www.google.com/serach?q=hello+universe
+5   Presiona "enter"
+
+Deberia ver nuevos resultados de busqueda para la consulta "hola mundo".
+Google eligio usar parametros de consulta para representar el calo de su consulta.
+Tiene sentido: cada pagina de resultados de busqueda es esencialmente 
+la misma pagina en la que respecta a la estructura y el formato;
+    solo muestra resultados diferentes segun la consulta de busqueda.
+*/
