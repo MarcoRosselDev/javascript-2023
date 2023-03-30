430622f6922343628c57fda8f1565f1430565517
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-29 21:08:51 -0300

    uri | uris parts | a

---
+++ HTTP-Networking/3-URI/3-url-parts/url-parts.js
@@ -0,0 +1,42 @@
+/* Further dissectiong a URL
+eng
+
+There are 8 main parts to a URL, 
+though not all the sections are always present. 
+Each piece plays a specific role in helping clients locate the specified resource.
+
+The 8 sections are:
+
+1   Protocol        http://
+2   Username        dragonslayer:
+3   Password        pwn3d@
+4   Domain          fantasyquest.com:
+5   Port            8080/
+6   Path            maps?
+7   Query           sort=rank#
+8   Fragment        id
+
+http://dragonslayer:pwn3d@fantasyquest.com:8080/maps?sort=rank#id
+
+----------------------------------------------------------------------
+spa
+
+Hay 8 partes principales en una URL,
+aunque no todas las secciones estan siempre presentes.
+cada pieza juega un papel especifico para ayudar a los clientes
+a localizar el recurso espacificado.
+
+las 8 secciones son:
+
+1   Protocol        http://
+2   Username        dragonslayer:
+3   Password        pwn3d@
+4   Domain          fantasyquest.com:
+5   Port            8080/
+6   Path            maps?
+7   Query           sort=rank#
+8   Fragment        id
+
+http://dragonslayer:pwn3d@fantasyquest.com:8080/maps?sort=rank#id
+
+*/
