021293b7b73709c830d382c8ae635c2925dd7f1c
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-31 15:10:01 -0300

    errors | bugs vs errors  2

---
+++ HTTP-Networking/5-errors/2-bugs-vs-errors/error-handling.js
@@ -0,0 +1,29 @@
+/* Error handling
+eng
+
+"Error handling" is code that can handle expected 
+edge cases in your program. 
+Error handling is an automated process that we design into our 
+production code to protect it from things like weak internet connections, 
+bad user input, or bugs in other people's code that we have to interface with.
+
+Examples of error handling:
+
+    Using a try/catch block to detect an issue with user input
+    Using a try/catch block to gracefully fail when no internet connection is available
+
+-----------------------------------------------------------------
+spa
+
+El "manejo de errores" es un codigo que puede manejar 
+los casos extremos espareados e su programa.
+El manejo de errores es un proceso automatizado que dise;amos 
+en nuestro codigo de produccion para protegerlo de cosas como conexiones de internet debiles,
+entradas de usuario incorrectas o errores en el codigo de otras personas con los que tenemos que interactuar.
+
+Ejemplos de manejo de errores:
+
+    Uso de un bloque de try/catch para detectar un problema con la entrada del usuario
+    Uso de un bloque de try/catch para fallar con gracia cuando no hay conexion a internet disponible.
+    
+*/
---
+++ HTTP-Networking/5-errors/2-bugs-vs-errors/resumen.js
@@ -0,0 +1,10 @@
+/* In short, don't use try/catch to try to handle bugs
+    En resumen, no use try/catch para tratar de manejar errores
+
+eng
+
+
+
+
+
+*/
