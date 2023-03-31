e00dfc4658f31134b10d9ae12283607e72f9ae28
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-31 16:16:36 -0300

    errors | .catch() .then()

---
+++ HTTP-Networking/5-errors/3-try-catch-async/catch-callback-on-promizes.js
@@ -0,0 +1,12 @@
+/* .catch() callback on promises
+eng
+
+The .catch() method works similarly to the .then() method, 
+but it fires when a promise is rejected instead of resolved.
+-----------------------------------------------------------
+spa
+
+El metodo .catch() funciona de manera similar al metodo .then(),
+pero se activa cuando se rechaza una promesa en lugar de resolverla.
+
+*/
---
+++ HTTP-Networking/5-errors/3-try-catch-async/try-catch-async.js
@@ -0,0 +1,21 @@
+/* async/await makes error hanling easier
+eng
+
+try and catch are the standard way to handle errors in JavaScript, 
+the trouble is, the original Promise API with .then didn't allow us 
+to make use of try and catch blocks.
+
+Luckily, the async and await keywords do allow it, 
+yet another reason to prefer the newer syntax.
+
+
+--------------------------------------------------------------------
+spa
+
+try y catch son la forma estandar de manejar errores en javaScript, 
+el problema es que la API original de Promise con .then no no permite 
+hacer uso de los bloques try y catch
+
+Afortunadamente, las palabras clave async y await lo permite,
+otra razon mas para preferir la sintaxis mas nueva.
+*/
