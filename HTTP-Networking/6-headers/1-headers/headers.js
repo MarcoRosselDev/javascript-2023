c5147b0b618561938a445d7c97979dcc531a8f50
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-31 16:30:53 -0300

    headers | headers

---
+++ HTTP-Networking/6-headers/1-headers/headers.js
@@ -0,0 +1,34 @@
+/* What are HTTP Headers?
+eng
+
+An HTTP header allows clients and servers to pass additional 
+information with each request or response. 
+Headers are just case-insensitive key-value pairs that pass 
+additional metadata about the request or response.
+
+HTTP requests from a web browser carry with them many headers, 
+including but not limited to:
+    The type of client (e.g. Google Chrome)
+    The Operating system (e.g. Windows)
+    The preferred language (e.g. US English)
+
+As developers, we can also define custom headers in each request.
+
+
+--------------------------------------------------------------------------
+spa
+
+Un encabezado HTTP permite a los clientes y servidores 
+pasar informacion adicional con cada solicitud o respuesta.
+Los encabezados son simplemente pares clave-valor que no distinguen entre 
+mayusculas y minusculas y transmiten metadatos adicionales sobre la solicitud o la respuesta.
+
+Las solicitudes HTTP de un navegador web llevan consigo muchos encabezados,
+incluidos, entre otros, los siguientes:
+    El tipo de cliente (por eje. Google Chrome)
+    El sistema operativo (por eje. Windows)
+    El idioma preferido (por eje. ingles de EE.UU.)
+
+Como desarrolladores, tambien podemos definir 
+encabezados personalizados en cada solicitud.
+*/
