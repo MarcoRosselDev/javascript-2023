004e4118711727609aa6ec01eaa6c4d63b36e428
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-31 16:34:30 -0300

    headers | headers API

---
+++ HTTP-Networking/6-headers/1-headers/headers-api.js
@@ -0,0 +1,18 @@
+/* Headers API
+eng
+
+The Headers API allows us to perform various actions 
+on our request and response headers such as retrieving, 
+setting, and removing them. 
+We can access the headers object through the 
+Request.headers and Response.headers properties.
+---------------------------------------------------------------------------
+spa
+
+La API del encabezado permite realizar varios acciones 
+en nuestros encabezados de solicitud y respuesta,
+como recuperarlos, configurarlos y eliminarlos.
+Podemos ecceder al objeto headers a traves de las 
+propiedades Request.headers y Response.headers.
+
+*/
