467441d0597ff2ac09a4c6e6703e32c66804b1e2
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-31 15:14:41 -0300

    errors | try/catch resumen

--- HTTP-Networking/5-errors/2-bugs-vs-errors/resumen.js
+++ HTTP-Networking/5-errors/2-bugs-vs-errors/resumen.js
@@ -3,8 +3,30 @@
 
 eng
 
+If your code has a bug, try/catch won't help you. 
+You need to just go find the bug and fix it.
 
+If something out of your control can produce issues in your code, 
+you should use try/catch or other error-handling logic to deal with it.
 
+For example, there could be a prompt in Fantasy Quest 
+for users to type in a new character name, 
+but we don't want them to use punctuation. 
+Validating their input and displaying an error message if something 
+is wrong with it would be a form of "error handling".
 
+------------------------------------------------------------
+spa
 
+Si su codigo tiene un error, try/catch no lo ayudara.
+Solo tienes que ir a buscar el error y arreglarlo.
+
+Si algo fuera de su control puede producri problemas en su codigo,
+debe usar try/catch u otra logica de manejo de errores para solucionarlo.
+
+Por ejemplo, podria haber un aviso en Fantasy Quest 
+para que los usuarios escriban un nuevo nombre de personaje,
+pero no queremos que usen puntuacion.
+Validar su entrada y mostrar un mensaje de error si algo 
+esta mal seria una forma de "manejo de errores".
 */
