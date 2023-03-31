92e3a92ffdf89a7562b62766033ec4689b43beeb
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-31 13:54:48 -0300

    errors | bugs vs errors

---
+++ HTTP-Networking/5-errors/2-bugs-vs-errors/bugs-vs-errors.js
@@ -0,0 +1,20 @@
+/* Bugs vs Errors
+eng
+Error handling via try/catch is not the same as debugging. 
+Likewise, errors are not the same as bugs.
+
+    Good code with no bugs can still produce errors that are gracefully handled
+    Bugs are, by definition, bits of code that aren't working as intended
+
+
+------------------------------------------------------------
+spa
+
+El manejo de errores a traves de try/catch no es lo mismo que la depuracion(debugging).
+Asimismo, los errores no son lo mismo que los insectos(bugs).
+
+    Un buen codigo sin errores aun puede producir errores que se manejan con gracia.
+    Los Bugs son, por definicion, fragmentos de codigo que no funcionan segun lo previsto.
+
+
+*/
