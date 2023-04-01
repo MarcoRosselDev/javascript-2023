b1bd637a79789a38d8194cd0eba31f9e724612d7
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-31 22:22:37 -0300

    JSON | sending json

---
+++ HTTP-Networking/7-json/3-sending-json/sending-json.js
@@ -0,0 +1,18 @@
+/* Sending JSON
+
+JSON isn't just something we get from the server, 
+we can also send JSON data!
+
+In JavaScript, two of the main methods we have access to are 
+JSON.parse(), and JSON.stringify().
+
+---------------------------------------------------
+spa
+
+JSON no es solo algo que obtenemos del servidor,
+tabien podemos enviar datos JSON !
+
+En javaScript, dos de los principales metodos a los que tenemos acceso son
+JSON.parse() y JSON.stringify()
+
+*/
