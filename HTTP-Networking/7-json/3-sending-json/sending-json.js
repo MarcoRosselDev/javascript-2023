908390bc8cca9fec09cb5060e5e917f82afaa0ad
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-01 18:46:27 -0300

    JSON | sending json 2

--- HTTP-Networking/7-json/3-sending-json/sending-json.js
+++ HTTP-Networking/7-json/3-sending-json/sending-json.js
@@ -16,3 +16,21 @@ En javaScript, dos de los principales metodos a los que tenemos acceso son
 JSON.parse() y JSON.stringify()
 
 */
+
+/* JSON.stringify()
+eng
+JSON.stringify() is particularly useful for sending JSON.
+
+As you may expect the JSON stringify() method does the opposite of parse. 
+It takes a JavaScript object or value as input and converts it into a string. 
+This is useful when we need to serialize the objects into strings to 
+send them to our server or store them in a database.
+-----------------------------------------------------
+spa
+
+JSON.stringify() es particularmete util para enviar JSON.
+
+Como es de esperar, el metodo JSON stringify() hace lo contrario al parse.
+Esto es util cuando necesitamos serializar los objetos en cadenas 
+para enviarlos a nuestro servidor o almacenarlos en una base de datos.
+*/
