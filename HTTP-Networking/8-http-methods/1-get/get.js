a8db79f79fcde128e0c473cc6c08a71b6721ed28
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-02 03:58:02 -0300

    http methods | get

---
+++ HTTP-Networking/8-http-methods/1-get/get.js
@@ -0,0 +1,36 @@
+/* HTTP Methods - GET
+eng
+
+HTTP defines a set of methods that we use every time we make a request. 
+We have used some of these methods in previous exercises, 
+but it's time we dive into them and understand the differences and 
+use cases behind the different methods.
+---------------------------------------------------------
+spa
+
+HTTP define un conjunto de metodos que usamos cada vez que hacemos una solicitud.
+Hemos usado algunos de estos metodos en ejercicios anteriores, 
+pero es hora de sumergirnos en ellos y comprender las diferencias y 
+los casos de uso detras de los diferentes metodos.
+*/
+
+//-------------------------------------------------------
+
+/* The GET method
+eng
+
+The GET method is used to 'get' a representation of a specified resource. 
+You are not taking the data away from the server, but rather getting a representation, 
+or copy, of the resource in its current state. 
+A get request is considered a safe method to call multiple times 
+because it doesn't alter the state of the server.
+
+---------------------------------------------------------
+spa
+
+El metodo GET se usa para "obtener" una representacion de un recurso especifico.
+No esta sacando los datos del servidor, sino obteniendo una representacion,
+o copia, del recurso en su estado actual.
+Una solicitud de obtencion se considera un metodo seguro para llamar varias veces 
+porque no altera el estado del servidor.
+*/
