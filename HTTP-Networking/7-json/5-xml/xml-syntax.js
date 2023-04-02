8ccff0bbd0676c1212d80b13e93a8c3d5707258b
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-02 01:55:26 -0300

    JSON | XML

---
+++ HTTP-Networking/7-json/5-xml/xml-syntax.js
@@ -0,0 +1,32 @@
+/* XML syntax
+eng
+
+XML is a markup language like HTML, 
+but it's more generalized in that it does not use predefined tags. 
+Just like how in JSON an objects' keys can be called anything, 
+XML tags can also have any name.
+----------------------------------------------------------------
+spa
+
+XML es un lenguaje de marcas como HTML,
+pero es mas generalizado porque no utiliza etiquetas predefinidas.
+Al igual que en JSON, las claves de los objetos se pueden llamar de cualquier manera,
+XML tambien puede tener cualquier nombre.
+
+*/
+
+// XML Example
+<root>
+    <id>1</id>
+    <genre>Action</genre>
+    <title>Iron Man</title>
+    <director>Jon Favreau</director>
+</root>
+
+// The same data but in JSON form:
+{
+    "id": "1",
+    "genre": "Action",
+    "title": "Iron Man",
+    "director": "Jon Favreau"
+}
---
+++ HTTP-Networking/7-json/5-xml/xml.js
@@ -0,0 +1,15 @@
+/* XML
+eng
+
+We can't talk about JSON without mentioning XML. Extensible Markup Language, 
+or XML is a text-based format for representing structured information, 
+just like JSON - it just looks a bit different.
+-------------------------------------------------
+spa
+
+No podemos hablar de JSON sin mencionar XML.
+El lenguaje de marcado extensible, o XML, 
+es un formato basado en texto para representar informacion estructurada,
+al igual que JSON, solo que se ve un poco diferente.
+
+*/
