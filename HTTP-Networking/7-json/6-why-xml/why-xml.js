812ec8526045c3fccce6493f409e9ee66525520a
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-02 02:03:48 -0300

    JSON | why xml

---
+++ HTTP-Networking/7-json/6-why-xml/why-xml.js
@@ -0,0 +1,33 @@
+/* Why use XML ?
+eng
+
+XML and JSON both accomplish similar tasks, 
+so which should you use?
+
+XML used to be used for the same things that today JSON is used for. 
+Configuration files, HTTP bodies, and other data-transfer 
+use cases can work just fine using JSON or XML. 
+My advice is that generally speaking if JSON works, 
+you should favor it over XML these days. 
+JSON is lighter-weight, easier to read, and has better support in most 
+modern programming languages.
+
+There are some cases where XML might still be the better, 
+or maybe even the necessary choice, but those cases will be rare.
+------------------------------------------------
+spa
+
+XML y JSON realizan tareas similares,
+entonces, cual deberia usar ?
+
+XML solia usarse para las misma cosas para las que hay se una JSON.
+Los archivos de configuracion, los cuerpos HTTP y otros casos de uso 
+de tranferencia de datos pueden funcionar perfectamente con JSON o XML.
+Mi consejo es que, en terminos generales, si JSON funciona,
+deberia preferirlo sobre XML estos dias.
+JSON es mas liviano, mas facil de leer y tiene mejor soporte en la mayoria 
+de los lenguajes de programacion modernos.
+
+Hay algunos casos en los que XML aun podria ser mejor, 
+o tal vez incluso la opcion nocesaria, pero esos casos seran raros.
+*/
