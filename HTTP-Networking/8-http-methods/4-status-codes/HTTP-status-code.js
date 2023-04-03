51a55bf7ae740bd6a2aa9c1729c7eab3d1554611
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-02 22:18:37 -0300

    http methods | status codes

---
+++ HTTP-Networking/8-http-methods/4-status-codes/HTTP-status-code.js
@@ -0,0 +1,19 @@
+/*  HTTP Status Codes
+eng
+
+Now that we understand how to write HTTP requests from scratch, 
+we need to learn how to ensure that the server is doing what we want. 
+Earlier in the course, we learned about how to access the browser's developer 
+tools and use those tools to inspect HTTP requests. 
+We can use that same process to check on the requests we are making and verify 
+what they are doing so we can address potential problems.
+---------------------------------------------------------------
+spa
+
+Ahora que entendemos como escribir solicitudes HTTP desde cero,
+debemos aprender como asegurarnos de que el servidor este haciendo lo qu queremos.
+Anteriormente en el curso, aprendimos como acceder a las herreamientas de desarrollo 
+del navegador y usar esas herramientas para inspeccionar las solicitudes HTTP.
+Podemos usar ese mismo proceso para verificar las solicitudes que estamos haciendo y 
+verificando para que podamos abordar posibles problemas.
+*/
