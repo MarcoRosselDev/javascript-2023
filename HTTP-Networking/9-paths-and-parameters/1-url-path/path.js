776b02e1c1f82c7076b0e41ecddb200af303be0f
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-03 20:12:27 -0300

    path and parameters | url

---
+++ HTTP-Networking/9-paths-and-parameters/1-url-path/path.js
@@ -0,0 +1,46 @@
+/* URL Paths
+eng
+
+The URL Path comes right after the domain (or port if one is provided) 
+in a URL string.
+
+In this URL, the path is /root/next:
+http://testdomain.com/root/next
+
+-------------------------------------------------
+spa
+
+La ruta de URL viene justo despues del dominio (o puerto si se proporciona uno)
+en una cadena de URL.
+
+En esta URL, la ruta es /root/next:
+http://testdomain.com/root/next
+
+*/
+
+
+/* What paths meant in the early internet
+eng
+
+In the early days of the internet, and sometimes still today, 
+many web servers simply served raw files from the server's file system. 
+For example, if I wanted you to be able to access some text documents, 
+I could start a web server in my documents directory. 
+If you made a request to my server you would be able to access different documents 
+by using the path that matched my local file structure.
+
+If I had a file in my local /documents/hello.txt, 
+you could access it by making a GET request to http://example.com/documents/hello.txt.
+-------------------------------------------------
+spa
+
+En los primeros dias de internet, y a veces todavia hoy,
+muchos servidores web simplemente servian archivos sin formato desde el sistema de archivos del servidor.
+Por ejemplo, si quisiera que pudiera acceder a algunos documentos de texto,
+podria iniciar un servidor web en mi directorio de documentos.
+Si realizo una solicitud a mi servidor, podria acceder a diferentes documentos utilizando 
+la ruta que coincidia con mi estructura de archivos local.
+
+Si tuviera un archivo en mi /documents/hello.txt,
+podria acceder a el haciendo una solicitud GET a http://example.com/documents/hello.txt.
+*/
