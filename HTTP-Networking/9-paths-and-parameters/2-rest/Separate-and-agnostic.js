3642def716fd7e41b4c99e8877f042845a58493b
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-03 23:46:52 -0300

    path and parameters | REST API | separate and agnostic

---
+++ HTTP-Networking/9-paths-and-parameters/2-rest/Separate-and-agnostic.js
@@ -0,0 +1,15 @@
+/* Separate and agnostic
+eng
+
+The big idea behind REST is that resources are transferred via well-recognized, 
+language-agnostic client/server interactions. 
+A RESTful style means the implementation of the client and server can be done independently of one another, 
+as long as some simple standards, like the names of the available resources, have been established.
+------------------------------------------------------------------------------
+spa
+
+La gran idea detras de REST es que los recurses se transfieren a traves de interacciones 
+cliente/servidor idndependientes del lenguaje bien reconocidas.
+Un estilo RESTful significa que la implementacion del cliente y el servidor se puede realizar de forma independiente, 
+siempre que se hayan establecido algunos estandares simple, como los nombres de los recursos disponibles.
+*/
