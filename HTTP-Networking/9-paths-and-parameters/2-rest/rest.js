38064a6818e65362c4647fffb1ef17571f54b4bf
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-03 22:00:01 -0300

    path and parameters | REST API

---
+++ HTTP-Networking/9-paths-and-parameters/2-rest/rest.js
@@ -0,0 +1,17 @@
+/* RESTful APIs
+eng
+
+Representational State Transfer, or REST, is a popular convention that HTTP servers follow. 
+Not all HTTP APIs are "REST APIs", or "RESTful", but it is very common.
+
+RESTful servers follow a loose set of rules that makes it easy to build reliable and predictable web APIs. 
+REST is more or less a set of conventions about how HTTP should be used.
+---------------------------------------------------------------------
+spa
+
+Representacion de transferencia de estado, o REST, es una convencion popular que siguen los servidores HTTP.
+No todas las APIs HTTP son "API REST" o "RESTful", pero es muy comun.
+
+Los servidores RESTful siguen un conjunto flexible de reglas que facilita la creacion de API web confiables y predecibles.
+REST es mas o menos un conjunto de convenciones sobre como se sebe usar HTTP.
+*/
