33ebfd4751f03b997a263e9b78c7cef1c75e7ba1
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-05 00:47:37 -0300

    path and parameters | documentation

---
+++ HTTP-Networking/9-paths-and-parameters/4-documentation/documentation.js
@@ -0,0 +1,33 @@
+/* The Documentation of an HTTP Server
+eng
+
+you may be wondering:
+    How the heck am I supposed to memorize how all these different servers work?
+
+The good news is that you don't need to. 
+When you work with a backend server, it's the responsibility of that server's developers to provide you with instructions, 
+or documentation that explains how to interact with it. 
+For example, the documentation should tell you:
+
+    The domain of the server
+    The resources you can interact with (HTTP paths)
+    The supported query parameters
+    The supported HTTP methods
+    Anything else you'll need to know to work with the server
+--------------------------------------------------------------------------
+spa
+
+te estaras preguntando:
+    Como diablos se supone que debo memorizar como funcionan todos estos servidores diferentes ?
+
+La buena noticia es que no lo necesitas.
+Cuando trabajas con un backend server, es responsabilidad de los desarrolladores de ese server 
+proporcionarle instrucciones o documentacion que explique como interactuar con el.
+Por ejemplo, la documentacion deberia decirte :
+
+    El dominio del servidor
+    Los recursos con los que puede interactuar (rutas HTTP)
+    Los parámetros de consulta admitidos
+    Los métodos HTTP admitidos
+    Cualquier otra cosa que necesite saber para trabajar con el servidor
+*/
