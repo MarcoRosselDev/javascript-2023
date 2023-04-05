6f2265663bc7b1a6727201cd8fabd74c7b07d86a
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-05 00:58:29 -0300

    path and parameters | multipels query parameters

---
+++ HTTP-Networking/9-paths-and-parameters/5-query-par-multiple/query-parameters-multiple.js
@@ -0,0 +1,26 @@
+/* Multiple Query Parameters
+eng
+We mentioned that query parameters are key/value pairs,
+that means we can have multiple pairs!
+
+http://example.com?firstName=lane&lastName=wagner
+
+In the example above:
+
+firstName = lane
+lastName = wagner
+The ? separates the query parameters from the rest of the URL. 
+The & is then used to separate every pair of query parameters after that. 
+-----------------------------------------------------------
+spa
+Mencionamos que los parametros de consulta son pares clave/valor,
+eso significa que podemos tener varios pares !
+
+http://example.com?firstName=lane&lastName=wagner
+
+En el ejemplo anterior:
+nombre = carril
+apellido = wagner
+El ? separa los parámetros de consulta del resto de la URL.
+El & se usa para separar cada par de parámetros de consulta después de eso.
+*/
