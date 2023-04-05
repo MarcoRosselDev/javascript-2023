cbb85470520e57be57296cb05ee5e5408853574f
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-04 22:38:20 -0300

    path and parameters | REST API | stateless

---
+++ HTTP-Networking/9-paths-and-parameters/2-rest/stateless.js
@@ -0,0 +1,17 @@
+/* Stateless
+eng
+
+A RESTful architecture is stateless, which means the server does not need to know what state the client is in, 
+nor does the client need to know what state the server is in. 
+Statelessness in REST is enforced by interacting with resources instead of commands. 
+Keep in mind, this doesn't mean the applications are stateless;
+    on the contrary, what would "updating a resource" even mean if the server wasn't keeping track of its state?
+------------------------------------------------------------------------------
+spa
+
+Una arquitectura RESTful no tiene estado, lo que significa que el servidor no necesita saber en que estado se encuentra el cliente,
+ni el cliente necesita saber en que estado se encuentra el servidor.
+La falta de estado en REST se aplica interactuando con recursos en lugar de comandos.
+Tenga en cuenta que esto no significa que las aplicacion no tengan estado;
+    por el contrario, que significa "actualizar un recurso" si el servidor no estuviera realizando un seguimiento de su estado ?
+*/
