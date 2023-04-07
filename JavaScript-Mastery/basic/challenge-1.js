d06059b49db601b1fa938f816f4fb28557956f12
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-04-06 22:33:44 -0400

    javaScript Mastery | basic | challenge 1

--- JavaScript-Mastery/basic/challenge-1.js
+++ JavaScript-Mastery/basic/challenge-1.js
@@ -8,4 +8,13 @@ Then add a script tag before the closing </body> tag and between the script tags
 Assign your first name to one and your last name to the other,
 then make javaScript display the value of each variable in the console.
 
+-------------------------------------------------------
+spa
+
+Cree un archivo HTML y establezca el título en "Desafíos básicos de JavaScript: Desafío 1"
+Luego agregue un <h1> y <h2> con esa información.
+
+Luego agregue una etiqueta de secuencia de comandos antes de la etiqueta de cierre </body> y entre las etiquetas de secuencia de comandos cree dos variables.
+Asigne su nombre a uno y su apellido a otro,
+luego haga que javaScript muestre el valor de cada variable en la consola.
 */
