6cc9e59ce0a33cfac150cf990648055c6a0a9205
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-08 19:55:53 -0300

    mastery javaSCript | challenge 9

---
+++ JavaScript-Mastery/basic/challenge-9/app.js
@@ -0,0 +1,24 @@
+const inputColor = document.getElementById('inputColor')
+const butonColor = document.getElementById('buttonEnviar')
+const divColor = document.getElementById('color')
+const then = document.getElementById('then')
+
+butonColor.addEventListener('click', function () {
+    const color = inputColor.value;
+
+    if (color == 'red' || 'blue' || 'yellow') {
+        divColor.style.backgroundColor = color
+        console.log('primer if');
+    } else if (color == 'orange' || 'green' || 'purple') {
+        divColor.style.backgroundColor = color
+        console.log('segundo if');
+    } else {
+        then.innerText = "no escribiste ninguna de las opciones"
+        console.log('tercer if');
+    }
+    
+}) 
+
+/* 1    ingrese un color en texto
+2   imprimir el color con el texto ? o un div con el color
+3   manejar los errores en caso de malos ingresos */
---
+++ JavaScript-Mastery/basic/challenge-9/challenge-9.js
@@ -0,0 +1,23 @@
+/*      Challenge 9
+eng
+
+Prompt the user for a color, then have the 
+console report if the user typed a primary color 
+(red, blue, or yellow) or a secondary color 
+(orange, green or purple) or none of these. 
+Handle the situation for both uppercase and 
+lowercase letters in the color the user types in.
+Hint: remember the || means 'or'
+
+------------------------------------------
+spa
+
+Solicite al usuario un color, luego haga que el
+informe de la consola si el usuario escribió un color primario
+(rojo, azul o amarillo) o un color secundario
+(naranja, verde o morado) o ninguno de estos.
+Manejar la situación tanto para mayúsculas como para
+minúsculas del color que escribe el usuario.
+Pista: recuerda el || significa 'o'
+
+*/
