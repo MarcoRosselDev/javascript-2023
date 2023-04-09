1b010f9a3d93d11318c0085a25c146b45eccbe7d
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-09 03:04:13 -0300

    mastery javaSCript | challenge 11

---
+++ JavaScript-Mastery/basic/challenge-11/challenge-11.js
@@ -0,0 +1,20 @@
+/*      Challenge 11
+eng
+
+For this challenge, change hello() so that it
+takes a parameter, and when you run the
+function, you pass in the name of a person
+and the function outputs the greeting to the
+console.
+
+--------------------------------------------------
+spa
+
+Para este desafío, cambie hello() para que
+toma un parámetro, y cuando ejecuta el
+función, se pasa en nombre de una persona
+y la función envía el saludo al
+consola.
+*/
+
+// aplica el resultado del desafio 10
