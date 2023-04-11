b5d4ab702bcb25ced39419df521b905c51b563fb
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-11 05:32:25 -0300

    mastery javaSCript | challenge 16

---
+++ JavaScript-Mastery/basic/challenge-16/challenge-16.js
@@ -0,0 +1,22 @@
+/*      Challenge 16
+spa
+
+Para este desafío, agregue la función que obtiene
+enteros aleatorios de la lección de funciones.
+Actualice la función de hospitalidad () para que,
+en lugar de recorrer toda la matriz,
+elige al azar un elemento de la matriz
+y ofrece eso al usuario.
+------------------------------------------------
+eng
+
+For this challenge, add the function that gets
+random integers from the functions lecture.
+Update the hospitality() function so that,
+instead of looping through the whole array,
+randomly picks one element from the array
+and offers that to the user.
+
+*/
+
+// ya lo aplique en el ejercicio 15
