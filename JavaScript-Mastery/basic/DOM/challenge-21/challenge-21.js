d4fd30c86abf9d93d269c76720af0c7b71f1c7a1
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-12 04:01:48 -0300

    mastry JavaScript | challenge 21

---
+++ JavaScript-Mastery/basic/DOM/challenge-21/challenge-21.js
@@ -0,0 +1,37 @@
+/*          Challenge 21
+eng 
+
+For this challenge, get all the links, then check
+to see if one of them is set to go to the
+wikipedia page on llamas, if it is, set the text
+of that link to "Llamas are cool!"
+Do you need a hint? Look at the next slide...
+
+Use these document methods and properties:
+.getElementsByTagName();
+.getAttribute();
+.innerHTML = "";
+You will have multiple anchor tags to check
+and you're looking to see if one of them
+matches, so that should tell you what logical
+flow control structures you need.
+
+----------------------------------------------
+spa
+
+Para este desafío, obtenga todos los enlaces, luego verifique
+para ver si uno de ellos está configurado para ir a la
+página de wikipedia sobre llamas, si lo es, establezca el texto
+de ese enlace a "¡Las llamas molan!"
+¿Necesitas una pista? Mira la siguiente diapositiva...
+
+Utilice estos métodos y propiedades de documento:
+.getElementsByTagName();
+.getAttribute();
+.innerHTML = "";
+Tendrá varias etiquetas de anclaje para verificar
+y estás mirando a ver si uno de ellos
+coincidencias, por lo que debería decirle qué lógica
+estructuras de control de flujo que necesita.
+
+*/
