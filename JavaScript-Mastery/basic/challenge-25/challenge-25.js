571b924084c7454bafbe369ab2eb239051882014
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-12 05:43:07 -0300

    mastry JavaScript | challenge 25-1

---
+++ JavaScript-Mastery/basic/challenge-25/challenge-25.js
@@ -0,0 +1,24 @@
+/*      Challenge 25
+
+For this challenge, add an
+additional button that removes the
+last paragraph from the div.
+
+*/
+
+const div = document.querySelector('div')
+
+// seleccionando botones sin id ni clases
+const btn = document.querySelectorAll('button')
+const addParagraph = btn[0]
+const removeParagraph = btn[1]
+
+addParagraph.addEventListener('click', function () {
+    const newP = document.createElement('p');
+    newP.innerText = 'A new paragraph ';
+    div.appendChild(newP);
+})
+
+removeParagraph.addEventListener('click', function () {
+})
+
