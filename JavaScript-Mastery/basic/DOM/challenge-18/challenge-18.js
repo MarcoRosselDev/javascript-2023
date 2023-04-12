ba6b60c5f01598038ff1dcb50903e89a221f20be
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-11 21:48:46 -0300

    mastry JavaScript | challenge 18

---
+++ JavaScript-Mastery/basic/DOM/challenge-18/challenge-18.js
@@ -0,0 +1,29 @@
+/*          Challenge 18
+spa
+
+Para este desafío, obtenga todos los párrafos en
+la página, asígnelos a una variable, luego use
+esa variable en un bucle para establecer el estilo.color
+propiedad a verde.
+Intenta hacer esto sin mirar la respuesta.
+en la siguiente diapositiva!
+
+---------------------------------------------------
+eng
+
+For this challenge, get all the paragraphs on
+the page, assign them to a variable, then use
+that variable in a loop to set the style.color
+property to green.
+Try to do this without looking at the answer
+on the next slide!
+*/
+
+(function () {
+    const p = document.querySelectorAll('p')
+    console.log(p);// [p, p, p, p]
+    // esto retorna una lista por lo que tenemois que iterar por ella,
+    // entonces en cada una podemos aplicar .style.color = "green"
+
+    p.forEach(e => e.style.color = 'green')
+})()
