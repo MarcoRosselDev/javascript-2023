ae42200ffbb54c023e5c64081a60d679cff5c329
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-14 07:02:26 -0300

    mastry JavaScript | image project | 2

--- JavaScript-Mastery/basic/imageProj/imgaProj.js
+++ JavaScript-Mastery/basic/imageProj/imgaProj.js
@@ -2,15 +2,30 @@ const imagenes = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg','image5.jp
 
 (function () {
     'stict mode';
-    const bodyy = document.querySelector('body');
+    const divv = document.querySelector('div');
     const imgElement = document.createElement('img');
+    let iterador = 1
 
-    imgElement.style.height = '400px'
-    imgElement.style.width = '400px'
+    imgElement.style.height = '300px';
+    imgElement.style.width = '300px';
+    imgElement.src = `image${iterador}.jpg`
+    
+    divv.appendChild(imgElement);
+    
+    const btns = document.querySelectorAll('button');
+    
+    const nextBtn = btns[0];
+    const lastBtn = btns[1];
+    
+    nextBtn.addEventListener('click', function () {
+        
+        if (iterador < imagenes.length) {
+            iterador++;
+            imgElement.src = `image${iterador}.jpg`
+        } else {
+            iterador = 1
+            imgElement.src = `image${iterador}.jpg`
+        }
 
-    console.log(bodyy);
-    //bodyy.append(imgElement)
-    //body.append(imgElement)
-    //body.appendChild(imgElement)
-
+    })
 })()
