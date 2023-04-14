4e13c62606536a6ef0babdd209fbd10ae150e0d5
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-13 22:42:02 -0300

    mastry JavaScript | challenge 30

---
+++ JavaScript-Mastery/basic/challenge-30/challenge-30.js
@@ -0,0 +1,44 @@
+/*      Challenge 30
+Change the HTML and CSS
+to this, then write a script
+that changes the class on
+the body tag based on the
+key that was pressed.
+
+If someone presses a
+different key, give them a
+nasty alert message.
+
+Use the switch statement
+again.
+*/
+
+const bodyy = document.querySelector('body');
+
+document.addEventListener('keydown', function(e) {
+    
+    const btnPressed = e.which;
+
+    switch (btnPressed) {
+        case 66:
+            bodyy.className = 'one'
+            break;
+        case 86:
+            bodyy.className = 'two'
+            break;
+        case 67:
+            bodyy.className = 'three'
+            break;
+        case 70:
+            bodyy.className = 'four'
+            break;
+        case 71:
+            bodyy.className = 'five'
+            break;
+    
+        default:
+            alert('preciona una letra de la lista !')
+            break;
+    }
+
+})
