f285e27ac9ac3e37bb26ba352182304b1cf98e7b
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-15 10:08:36 -0400

    mastry JavaScript | jquery | challenge 4

---
+++ JavaScript-Mastery/jquery-animation/challengers/4/challenge-4.js
@@ -0,0 +1,9 @@
+/*      Challenge 4
+Use jQuery to change the font size to 24 pixels
+and the color to red for the paragraphs inside the
+div, but not the one outside the div.
+*/
+
+(function () {
+    $('#main p').css({'color':'red','font-size':'24px'})
+})()
