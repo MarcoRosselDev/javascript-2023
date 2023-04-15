8ca85f39ddcb366e9d9a5bb04d52bf902b5adcf5
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-15 11:02:52 -0400

    mastry JavaScript | jquery | challenge 8

---
+++ JavaScript-Mastery/jquery-animation/challengers/8/challenge-8.js
@@ -0,0 +1,10 @@
+/*      Challenge 8
+Use jQuery to change the color of every other
+paragraph inside the div to red.
+*/
+
+(function () {
+    'use strict';
+
+    $('#main p').css('color','red');
+})()
