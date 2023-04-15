7e0fd228c53436867c633ffd1bc930618a6b3088
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-15 05:45:42 -0400

    mastry JavaScript | jquery | challenge 2

---
+++ JavaScript-Mastery/jquery-animation/challengers/2/challenge-2.js
@@ -0,0 +1,10 @@
+/*     Challenge 2
+Use jQuery to change the size of the text in the
+paragraph to 24px.
+*/
+
+(function () {
+    'strict mode';
+    $(document.getElementById('paragraph')).css('font-size', '24px');
+    console.log(document.getElementById('paragraph').style.fontSize);
+})()
