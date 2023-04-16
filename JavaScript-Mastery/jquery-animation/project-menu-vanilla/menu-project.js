45af778a0616979cdeefb9198c685da2332c651a
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-16 09:21:26 -0400

    mastry JavaScript | jquery | menu project | 3

--- JavaScript-Mastery/jquery-animation/project-menu-vanilla/menu-project.js
+++ JavaScript-Mastery/jquery-animation/project-menu-vanilla/menu-project.js
@@ -3,24 +3,22 @@
 
     const allUlLi = document.querySelectorAll('ul li ul');
     const li = document.querySelectorAll('.menulink')
-    console.log(li); // devuelve un arreglo
-
-    /* li.addEventListener('click', function () {
-        console.log('clicked');
-    }) */
+    // console.log(li); // devuelve un arreglo
 
     // iteramos por el arreglo y aplicamos escucha de click
     for (let i = 0; i < li.length; i++) {
-        li[i].addEventListener('click', function () {
-            console.log('click');
+        li[i].addEventListener('click', function (e) {
+            e.preventDefault();
+
+            let esteMenu = this.parentNode.querySelector('ul');
+            console.log(esteMenu.innerHTML);
+            //li[i].className = 'show-menu'
         })
     }
 
     console.log(allUlLi[0].innerHTML);
 
     for (let i = 0; i < allUlLi.length; i++) {
-        allUlLi[i].addEventListener('click', function () {
-            console.log('clicked');
-        })
+        allUlLi[i].className = 'hide-menu';
     }
 })()
