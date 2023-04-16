029db8625690aab2fb12bc64bb6284977c7ed0e9
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-16 14:28:29 -0400

    mastry JavaScript | jquery | menu project reminder | resumen error cometido

--- JavaScript-Mastery/jquery-animation/reminder-menu-vanilla/app.js
+++ JavaScript-Mastery/jquery-animation/reminder-menu-vanilla/app.js
@@ -4,7 +4,13 @@
     const menuLinks = document.querySelectorAll('.menulink');
     
     function hideMenu() {
+        // ul ( li ( ul ( li ) ) ) ==> puede venir otra ul 
+        // por eso ul li ul, y no li ul li, que fue el error cometido en este intento
+        /* Conclucion:
+            las class from css se aplican en el ul no en cada li
+            por lo que iteramos en ul y inyectamos la case show-menu or hide-menu */
         const liss = document.querySelectorAll('ul li ul');
+
         for (let i = 0; i < liss.length; i++) {
             liss[i].className = 'hide-menu'
         }
@@ -16,7 +22,6 @@
         menuLinks[i].addEventListener('click', function (e) {
             e.preventDefault()
             const liMenu = this.parentNode.querySelector('ul');
-            console.log(liMenu);
 
             if (liMenu.className == 'hide-menu') {
                 hideMenu()
