9c1dd281d0029b8bd450b19cd0c7072c3cb2a3fa
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-18 09:33:56 -0400

    mastry JavaScript | jquery | menu project reminder | 7-break

--- JavaScript-Mastery/jquery-animation/menu-project/app.js
+++ JavaScript-Mastery/jquery-animation/menu-project/app.js
@@ -3,10 +3,19 @@
 
     const allUl = document.querySelectorAll('.ul');
     const allLi = document.querySelectorAll('ul li ul li')
+// primero ocultamos todas las sub-listas
 
-    console.log(allLi);
+function cerrarTodo() {
+    for (let i = 0; i < allLi.length; i++) {
+        allLi[i].className = 'ocultar-menu'
+    }
+}
+cerrarTodo()
+
+// mostramos  y ocultamos si click en menu x
     for (let i = 0; i < allUl.length; i++) {
         allUl[i].addEventListener('click', function () {
+
             const esteMenu = this.parentElement;
             const a = esteMenu.querySelectorAll('li')
             for (let f = 0; f < a.length; f++) {
@@ -14,15 +23,13 @@
                 if (a[f].className == 'mostrar-menu') {
                     a[f].className = 'ocultar-menu'
                 } else{
+                    cerrarTodo()
                     a[f].className = 'mostrar-menu';
                 }
             }
+        // preguntamos si alugn otro menu esta abierto
+        // si es true lo ocultamos y mostramos este
+
         })
     }
-
-    for (let i = 0; i < allLi.length; i++) {
-        allLi[i].className = 'ocultar-menu'
-        
-    }
-    console.log(allUl);
 })()
