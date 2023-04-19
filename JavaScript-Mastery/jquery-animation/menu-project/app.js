6562635023b77592cce091ee302fd3a41d86f924
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-19 08:04:07 -0400

    mastry JavaScript | jquery | menu project reminder | 9

--- JavaScript-Mastery/jquery-animation/menu-project/app.js
+++ JavaScript-Mastery/jquery-animation/menu-project/app.js
@@ -1,36 +1,25 @@
 (function () {
     'use strict';
 
-    const allUl = document.querySelectorAll('.ul');
-    const allLi = document.querySelectorAll('ul li ul')
-// primero ocultamos todas las sub-listas
+    const menu = document.querySelectorAll('.ul');
+    const allLi = document.querySelectorAll('ul li ul');
 
-function cerrarTodo() {
     for (let i = 0; i < allLi.length; i++) {
         allLi[i].className = 'ocultar-menu'
     }
-}
-cerrarTodo()
+    
+    for (let i = 0; i < menu.length; i++) {
+        menu[i].addEventListener('click', function (e) {
+            e.preventDefault
 
-// mostramos  y ocultamos si click en menu x
-    for (let i = 0; i < allUl.length; i++) {
-        allUl[i].addEventListener('click', function () {
-
-            console.log('clicked');
-            const esteMenu = this.parentNode.children;
-            console.log(esteMenu);
-            esteMenu.querySelectorAll('li')
-            /* for (let f = 0; f < a.length; f++) {
-
-                if (a[f].className == 'ocultar-menu') {
-                    a[f].className = 'mostrar-menu'
-                } else{
-                    a[f].className = 'ocultar-menu';
-                }
-            } */
-        // preguntamos si alugn otro menu esta abierto
-        // si es true lo ocultamos y mostramos este
-
+            const este = this.querySelectorAll('li');
+            
+            for (let h = 0; h < este.length; h++) {
+                este[h].className = 'mostrar-menu'
+                
+            }
         })
     }
+
+
 })()
