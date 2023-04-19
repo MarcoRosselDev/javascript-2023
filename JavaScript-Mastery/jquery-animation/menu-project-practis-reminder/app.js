ec92ed687600472939bb9ad72a45ffa838d27216
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-19 15:39:36 -0400

    mastry JavaScript | jquery | menu project reminder | 1.5

--- JavaScript-Mastery/jquery-animation/menu-project-practis-reminder/app.js
+++ JavaScript-Mastery/jquery-animation/menu-project-practis-reminder/app.js
@@ -2,15 +2,29 @@
     'use strict';
 
     const liSelect = document.querySelectorAll('.selectLi');
+    const allLi = document.querySelectorAll('ul li ul');
+
+    console.log(liSelect);
+    // primero ocultamos todas las listas
+    allLi.forEach(e => e.className = 'hide-menu')
     /* liSelect.forEach((e)=> e.addEventListener('click', (f)=>{
         console.log(f);
         console.log(`clicked from menu ` );
     })); */
+    
+    /* liSelect.forEach(e => e.addEventListener('click', (j)=>{
+        const este = j.this;
+        console.log(este);
+    })) */
 
-    for (let i = 0; i < liSelect.length; i++) {
+    liSelect.forEach(i => i.addEventListener('click', function(){
+        this.parentElement.querySelector('ul').className = 'show-menu';
+    }))
+
+    /* for (let i = 0; i < liSelect.length; i++) {
         liSelect[i].addEventListener('click', function () {
-            console.log('clicked aaa');
+            const este = this.parentElement.querySelector('ul');
+            este.className = 'show-menu'
         })
-        
-    }
+    } */
 })()
