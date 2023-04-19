7e633bbee93fb316b041c79bcb301a1f7e16d595
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-19 15:20:43 -0400

    mastry JavaScript | jquery | menu project reminder | 1.4

--- JavaScript-Mastery/jquery-animation/menu-project-practis-reminder/app.js
+++ JavaScript-Mastery/jquery-animation/menu-project-practis-reminder/app.js
@@ -2,5 +2,15 @@
     'use strict';
 
     const liSelect = document.querySelectorAll('.selectLi');
-    liSelect.forEach((e)=> console.log(e));
+    /* liSelect.forEach((e)=> e.addEventListener('click', (f)=>{
+        console.log(f);
+        console.log(`clicked from menu ` );
+    })); */
+
+    for (let i = 0; i < liSelect.length; i++) {
+        liSelect[i].addEventListener('click', function () {
+            console.log('clicked aaa');
+        })
+        
+    }
 })()
