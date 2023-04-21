ade3b54c7311c13fa0ce4dbc14bba561398dc7ab
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-21 19:37:18 -0400

    mastry JavaScript | jquery | toggle text project | 7

--- JavaScript-Mastery/jquery-animation/toggle-text/app.js
+++ JavaScript-Mastery/jquery-animation/toggle-text/app.js
@@ -1,38 +1,32 @@
 (function () {
     'use strict';
-
     const allSections = document.querySelectorAll('.toggle');
+    const allPara = document.querySelectorAll('.allPara');
+    let parrafo = 0;
 
-    // primero ocultamos todos menos el parrafo 1 que esta por defecto abierto
-    //allSections[0].className += ' show-text'
-    const parrafo1 = document.querySelector('.parrafo1');
-    const parrafo2 = document.querySelector('.parrafo2');
-    const parrafo3 = document.querySelector('.parrafo3');
-    let parrafo = 1;
-
-    parrafo1.addEventListener('click', function () {
-        /* this.style.backgroundColor = 'red' */
-        parrafo = 1;
-        asq();
-    })
-    parrafo2.addEventListener('click', function () {
-        parrafo = 2;
-        asq();
-    })
-    parrafo3.addEventListener('click', function () {
-        parrafo = 3;
-        asq();
-    })
+    for (let i = 0; i < allPara.length; i++) {
+        allPara[i].addEventListener('click', function () {
+            this.className += ' green'
+            //this.style.backgroundColor = 'red';
+            parrafo = i;
+            asq()
+        })
+        
+    }
+    function hideText() {
+        allSections.forEach(e => e.className = 'hide-text');
+    }
 
     function asq() {
-        if (parrafo == 1) {
-            allSections.forEach(e => e.className = 'hide-text');
+        if (parrafo == 0) {
+            hideText()
             allSections[0].className = 'show-text';
-        } else if (parrafo == 2) {
-            allSections.forEach(e => e.className = 'hide-text');
+
+        } else if (parrafo == 1) {
+            hideText()
             allSections[1].className = 'show-text';
-        } else if (parrafo == 3) {
-            allSections.forEach(e => e.className = 'hide-text');
+        } else if (parrafo == 2) {
+            hideText()
             allSections[2].className = 'show-text';
         }
     }
