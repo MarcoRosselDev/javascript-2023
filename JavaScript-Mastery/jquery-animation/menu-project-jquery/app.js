a03b5dec9f8d41442f8178a6dd898d00554187a6
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-20 21:04:02 -0400

    mastry JavaScript | jquery | menu project jquey 3

--- JavaScript-Mastery/jquery-animation/menu-project-jquery/app.js
+++ JavaScript-Mastery/jquery-animation/menu-project-jquery/app.js
@@ -10,6 +10,8 @@
 
     $('ul li ul').hide()
     $('.liClass').click(function () {
-        console.log($(this).next('ul').toggle());
+        const thisMenu = $(this).next('ul');
+        $('ul li ul').not(thisMenu).hide();
+        thisMenu.toggle();
     })
 })()
