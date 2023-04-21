92bfc62d554e28a95662f1c1a2aebd393ebeed2e
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-21 15:24:09 -0400

    mastry JavaScript | jquery | toggle text project | 5

--- JavaScript-Mastery/jquery-animation/toggle-text/app.js
+++ JavaScript-Mastery/jquery-animation/toggle-text/app.js
@@ -2,17 +2,40 @@
     'use strict';
 
     const allSections = document.querySelectorAll('.toggle');
-    console.log(allSections);
 
-    allSections.forEach(e => e.className += ' hide-text');
-    console.log();
-
+    // primero ocultamos todos menos el parrafo 1 que esta por defecto abierto
     //allSections[0].className += ' show-text'
-    
+    const parrafo1 = document.querySelector('.parrafo1');
     const parrafo2 = document.querySelector('.parrafo2');
-    parrafo2.addEventListener('click', function () {
-        allSections.forEach(e => e.className += ' hide-text');
-        allSections[1].className += ' show-text';
+    const parrafo3 = document.querySelector('.parrafo3');
+    let parrafo = 1;
 
+    parrafo1.addEventListener('click', function () {
+        /* this.style.backgroundColor = 'red' */
+        parrafo = 1;
+        asq();
     })
+    parrafo2.addEventListener('click', function () {
+        parrafo = 2;
+        asq();
+    })
+    parrafo3.addEventListener('click', function () {
+        parrafo = 3;
+        asq();
+    })
+
+    function asq() {
+        if (parrafo == 1) {
+            allSections.forEach(e => e.className = 'hide-text');
+            allSections[0].className = 'show-text';
+        } else if (parrafo == 2) {
+            allSections.forEach(e => e.className = 'hide-text');
+            allSections[1].className = 'show-text';
+        } else if (parrafo == 3) {
+            allSections.forEach(e => e.className = 'hide-text');
+            allSections[2].className = 'show-text';
+        }
+    }
+    asq()
+
 })()
