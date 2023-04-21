d4b07aff6c88a5f21905f6c9f32c16efebda3d98
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-20 21:01:29 -0400

    mastry JavaScript | jquery | menu project jquey 2

--- JavaScript-Mastery/jquery-animation/menu-project-jquery/app.js
+++ JavaScript-Mastery/jquery-animation/menu-project-jquery/app.js
@@ -1,5 +1,15 @@
 (function () {
     'use strict';
 
-    
+    //$('.liClass').next().css('display', 'none');
+
+    /* $('.liClass').click(function (e) {
+        e.preventDefoult
+        console.log(this);
+    }) */
+
+    $('ul li ul').hide()
+    $('.liClass').click(function () {
+        console.log($(this).next('ul').toggle());
+    })
 })()
