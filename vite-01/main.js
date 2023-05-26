debcd5993da33fc91f82e058aa7a90a8cb53f8e2
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-26 19:20:39 -0400

    vite prject 01  | 05

--- vite-01/main.js
+++ vite-01/main.js
@@ -1,5 +1,11 @@
 import './style.css';
 
+const playButton = document.querySelector('.ball');
+playButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('play ball clicked');
+})
+
 /* document.querySelector('#app').innerHTML = `
 <div class="main-div">
   <p class="read-the-docs">
