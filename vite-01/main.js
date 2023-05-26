fb9f98e92536d35ed103b1bf9b949f05c3f43a7b
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-26 18:34:47 -0400

    vite prject 01  | 02

--- vite-01/main.js
+++ vite-01/main.js
@@ -1,9 +1,13 @@
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
+const a = document.querySelectorAll('.app');
+
+a.forEach(e =>{
+  e.innerHTML = `
+  <div class="main-div">
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
 `;
+});
