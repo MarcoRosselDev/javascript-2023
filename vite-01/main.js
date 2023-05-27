4e8cedabe8656662482647fa55ac8e64ace02503
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-27 11:49:52 -0400

    vite prject 01  | 08

--- vite-01/main.js
+++ vite-01/main.js
@@ -6,8 +6,22 @@ const newNumber = document.querySelector('.newNumber');
 const player1Dom = document.querySelector('.player1');
 const cpuDom = document.querySelector('.cpu');
 
-const player1 = [2, 4, 5,7,10, 14,16,19, 20, 21,22,24,26, 29,30]
-const cpu = [2, 4, 5,7,6, 10,16,15, 9, 11,20,21,24, 26,28]
+const player1 = [2, 4, 5,7,10, 14,16,19, 20, 21,22,24,26, 29,30];
+const cpu = [1, 3, 5,7,8, 10,13,15, 19,20,21, 23,24, 26,28];
+
+function print(arr) {
+  let domPrint = '';
+  for (let i = 0; i < arr.length; i++) {
+    domPrint += `<div>${arr[i]}</div>`;
+  }
+  return domPrint;
+}
+
+const funPrint = print(player1);
+const cpuPrint = print(cpu)
+player1Dom.innerHTML = funPrint;
+cpuDom.innerHTML = cpuPrint;
+
 
 playButton.addEventListener('click', function (e) {
   e.preventDefault();
