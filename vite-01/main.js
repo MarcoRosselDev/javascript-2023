2207ae7fc65688263c71b809b54e27ab3f5d08d5
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-27 13:37:35 -0400

    vite prject 01  | 09

--- vite-01/main.js
+++ vite-01/main.js
@@ -1,13 +1,14 @@
 import './style.css';
 
-let randomNumber = [1, 4, 5];
 const playButton = document.querySelector('.ball');
-const newNumber = document.querySelector('.newNumber');
+const newNumber = document.querySelector('.numberPass');
 const player1Dom = document.querySelector('.player1');
 const cpuDom = document.querySelector('.cpu');
 
 const player1 = [2, 4, 5,7,10, 14,16,19, 20, 21,22,24,26, 29,30];
 const cpu = [1, 3, 5,7,8, 10,13,15, 19,20,21, 23,24, 26,28];
+let bingo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+let bingoPass = []
 
 function print(arr) {
   let domPrint = '';
@@ -25,9 +26,13 @@ cpuDom.innerHTML = cpuPrint;
 
 playButton.addEventListener('click', function (e) {
   e.preventDefault();
-  randomNumber.push(Math.random() * 50)
-  console.log('play ball clicked');
-  console.log(randomNumber[0]);
+  const randomNumber = Math.round(Math.random() * bingo.length);
+  const sliceBingo = bingo.splice(randomNumber, 1 );
+  console.log(sliceBingo, 'sliceBingo value');
+  console.log(bingo, 'bingo value');
+  bingoPass.push(sliceBingo);
+  const bingoDom = print(bingoPass);
+  newNumber.innerHTML = bingoDom;
 })
 
 //newNumber.innerHTML = `<div>${randomNumber[0]}</div>`;
