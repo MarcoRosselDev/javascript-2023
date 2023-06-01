dd57c28ce6007fa6f285f7ccd90ab62640656720
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-31 23:12:18 -0400

    reminder 21 05 23 2

--- exercises/reminder/app.js
+++ exercises/reminder/app.js
@@ -2,4 +2,17 @@
     'use strict';
 
     console.log('se ejecuto automaticamente cuando se cargo la pagina ');
+
+
+    const body = document.getElementById('body');
+    body.innerHTML = `
+    <form>
+        <input type="text">
+        <button class="btnSend">send</button>
+    </form>`;
+
+
+    // quiero que cuando se cargue la paguina se cargue el formulario desde javaScript!
+
+
 })()
