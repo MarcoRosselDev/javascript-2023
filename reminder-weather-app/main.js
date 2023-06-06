a490313cada4491bf77effba55c27333161810b4
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-05 20:16:20 -0400

    reminder-weather app | 03

--- reminder-weather-app/main.js
+++ reminder-weather-app/main.js
@@ -4,8 +4,12 @@ console.log(import.meta.env.VITE_WEATHER_KEY);
 
 document.querySelector('#app').innerHTML = `
   <div>
+
+
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
+    <input type="text" placeholder="Write some city...">
+    <button id="weather">weather</button>
   </div>
 `;
