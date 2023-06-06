f0296a5a5ce215eff65b8d2b1d9a53bd90b6202d
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-05 20:26:48 -0400

    reminder-weather app | 04

--- reminder-weather-app/main.js
+++ reminder-weather-app/main.js
@@ -1,11 +1,9 @@
 import './style.css';
 
-console.log(import.meta.env.VITE_WEATHER_KEY);
+const apiKey = import.meta.env.VITE_WEATHER_KEY
 
 document.querySelector('#app').innerHTML = `
   <div>
-
-
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
@@ -13,3 +11,24 @@ document.querySelector('#app').innerHTML = `
     <button id="weather">weather</button>
   </div>
 `;
+
+const input = document.querySelector('input');
+const weatherBtn = document.querySelector('#weather');
+
+weatherBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  console.log(input.value);
+  input.value = '';
+
+  async function weatherFunc(city) {
+    try {
+      const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`);
+      const data = await request.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  weatherFunc();
+
+})
