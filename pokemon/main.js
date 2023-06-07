f16a8da1100db87ae7620dfc5b8053fc2a125638
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-06 23:28:36 -0400

    pokemon api | 05

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,5 +1,5 @@
 import './style.css';
-//import { pokemon } from './pokemon.js';
+import { pokemon } from './pokemon.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -18,17 +18,6 @@ const pokeSearch = document.querySelector('.pokeSearch');
 pokeSearch.addEventListener('click', function (event) {
   event.preventDefault();
 
-  async function pokemon(name) {
-
-    try {
-        const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
-        const data = await request.json();
-
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
-};
-
+  // 
   pokemon(input.value);
 })
--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -1,7 +1,7 @@
 export async function pokemon(name) {
 
     try {
-        const request = await fatch(`https://pokeapi.co/api/v2/ability/${name}/`);
+        const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
         const data = await request.json();
 
         console.log(data);
