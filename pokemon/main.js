af3c9268fdd01ffcfc6918da39279ff8c247cd0e
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-06 23:05:19 -0400

    pokemon api | 03

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,10 +1,25 @@
 import './style.css';
-import { pokemon } from './pokemon.js'
+import { pokemon } from './pokemon.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <p>PokeAPI</p>
+
+    <form action="">
+      <input type="text">
+      <button class="pokeSearch">Search</button>
+    </form>
   </div>
 `;
+
+const input = document.querySelector('input');
+const pokeSearch = document.querySelector('.pokeSearch');
+
+pokeSearch.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  // look the poke api documentation to search a pokemon and print her img into a new div
+
+  console.log(input.value);
+  input.value = '';
+})
--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -1,4 +1,6 @@
 export function pokemon(params) {
+
+    
     console.log('pokemon function');
     // try catch ...
 };
